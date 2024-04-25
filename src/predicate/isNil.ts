22ada029f69acc3abba6cb5ae388d048de67af13
raon0211 <raon0211@toss.im>
2024-04-25 22:21:23 +0900

    Add isNil and isNotNil

--- src/predicate/isNil.ts
+++ src/predicate/isNil.ts
@@ -1,3 +1,8 @@
-export function isNil() {
-  throw new Error('Not implemented');
+/**
+ * @name isNil
+ * Checks if the given value is null or undefined.
+ * @param x The value to test if it is null or undefined
+ */
+export function isNil(x: unknown): x is null | undefined {
+  return x == null || x == undefined;
 }
--- src/predicate/isNotNil.ts
+++ src/predicate/isNotNil.ts
@@ -1,3 +1,14 @@
-export function isNotNil() {
-  throw new Error('Not implemented');
+/**
+ * @name isNotNil
+ * Checks if the given value is not null nor undefined.
+ * The main use of this function is to used with TypeScript as a type predicate.
+ * @param x The value to test if it is not null nor undefined
+ * @example
+ * // Here the type of `arr` is (number | undefined)[]
+ * const arr = [1, undefined, 3];
+ * // Here the type of `result` is number[]
+ * const result = arr.filter(isNotNil);
+ */
+export function isNotNil<T>(x: T | null | undefined): x is T {
+  return x != null && x != undefined;
 }
