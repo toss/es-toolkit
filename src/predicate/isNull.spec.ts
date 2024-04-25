84166f9ea3a33134fca87e3be3e1ca816717019e
raon0211 <raon0211@toss.im>
2024-04-25 22:24:26 +0900

    Add isNull and isUndefined

---
+++ src/predicate/isNull.spec.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest"
+import { isNull } from "./isNull"
+
+describe('isNull', () => {
+  it('returns true if the value is null', () => {
+    expect(isNull(null)).toBe(true);
+  });
+
+  it('returns false if the value is not null', () => {
+    expect(isNull(undefined)).toBe(false);
+    expect(isNull('')).toBe(false);
+    expect(isNull(123)).toBe(false);
+  })
+
+  it('can be used with TypeScript as a type predicate', () => {
+    const arr = [1, 2, null, 4, undefined];
+
+    const result = arr.filter(isNull);
+
+    // Here the type of result should be `null[]`.
+    expect(result).toStrictEqual([null]);
+  })
+})
--- src/predicate/isNull.ts
+++ src/predicate/isNull.ts
@@ -1,3 +1,8 @@
-export function isNull() {
-  throw new Error('Not implemented');
+/**
+ * @name isNull
+ * Checks if the given value is null.
+ * @param x The value to test if it is null
+ */
+export function isNull(x: unknown): x is null {
+  return x === null;
 }
