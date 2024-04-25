cdeddedc1bb893c068eece3d967bf6b1d4b68cd7
raon0211 <raon0211@toss.im>
2024-04-25 22:29:19 +0900

    Add docs to intersection

--- src/array/intersection.ts
+++ src/array/intersection.ts
@@ -2,6 +2,8 @@
  * @name intersection
  * @description
  * Returns the intersection of two arrays.
+ * @param firstArr The first array to get the intersection
+ * @param secondArr The second array to get the intersection
  * @example
  * intersection([1, 2], [1, 3]) === [1]
  * intersection([1, 2, 3], [2, 3, 4]) === [2, 3]
