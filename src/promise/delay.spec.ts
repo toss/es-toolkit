227931b8c68120fc8dd5d4a1bdd042eecea3d0de
raon0211 <raon0211@toss.im>
2024-05-13 12:46:21 +0900

    Add delay

---
+++ src/promise/delay.spec.ts
@@ -0,0 +1,13 @@
+import { performance } from 'node:perf_hooks';
+import { describe, expect, it } from "vitest";
+import { delay } from './delay';
+
+describe('delay', () => {
+  it('pauses an async function for a given time', async () => {
+    const start = performance.now()
+    await delay(100);
+    const end = performance.now()
+
+    expect(end - start).greaterThanOrEqual(100)
+  });
+})
--- src/promise/delay.ts
+++ src/promise/delay.ts
@@ -1,3 +1,10 @@
-export function delay() {
-  throw new Error('Not implemented');
+/**
+ * @name delay
+ * Pause the execution of a function for given milliseconds.
+ * @param ms The amount of time to delay the function.
+ */
+export function delay(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
