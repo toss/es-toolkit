cd96bbb3a1209f60096023a48ac381aa4979a9fd
raon0211 <raon0211@toss.im>
2024-05-16 22:42:56 +0900

    Add shuffle

--- src/array/shuffle.ts
+++ src/array/shuffle.ts
@@ -1,3 +1,18 @@
-export function shuffle() {
-  throw new Error('Not implemented');
+/**
+ * @name shuffle
+ * @description
+ * Randomize the order of an array.
+ * @param arr The array to shuffle
+ */
+export function shuffle<T>(arr: T[]): T[] {
+  /**
+   * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+   */
+  for (let i = arr.length - 1; i >= 1; i--) {
+    const j = Math.floor(Math.random() * i);
+    
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
 }
