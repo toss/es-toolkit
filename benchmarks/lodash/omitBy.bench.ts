f8714cf84eed57a5558b83863ea83a7c9c37c0ae
raon0211 <raon0211@toss.im>
2024-05-26 15:05:29 +0900

    omitBy

---
+++ benchmarks/lodash/omitBy.bench.ts
@@ -0,0 +1,17 @@
+import { bench, describe } from 'vitest';
+import { omitBy as omitByToolkit } from 'es-toolkit';
+import { omitBy as omitByLodash } from 'lodash';
+
+describe('omitBy', () => {
+  bench('es-toolkit', () => {
+    const obj = { a: 1, b: 'omit', c: 3 };
+    const shouldOmit = (value: number | string, key: string) => typeof value === 'string';
+    omitByToolkit(obj, shouldOmit);
+  })
+
+  bench('lodash', () => {
+    const obj = { a: 1, b: 'omit', c: 3 };
+    const shouldOmit = (value: number | string, key: string) => typeof value === 'string';
+    omitByLodash(obj, shouldOmit);
+  })
+});
--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -50,6 +50,7 @@ export default defineConfig({
             text: 'Object Utilities',
             items: [
               { text: 'omit', link: '/reference/object/omit' },
+              { text: 'omitBy', link: '/reference/object/omitBy' },
               { text: 'pick', link: '/reference/object/pick' },
             ]
           },
