d7f0acdd42979850459b53fd18a8dcbda6da7562
raon0211 <raon0211@toss.im>
2024-05-26 14:44:23 +0900

    isNil

---
+++ benchmarks/lodash/isNil.bench.ts
@@ -0,0 +1,19 @@
+import { bench, describe } from 'vitest';
+import { isNil as isNilToolkit } from 'es-toolkit';
+import { isNil as isNilLodash } from 'lodash';
+
+describe('isNil', () => {
+  bench('es-toolkit', () => {
+    isNilToolkit(null);
+    isNilToolkit(undefined);
+    isNilToolkit(123);
+    isNilToolkit([1, 2, 3]);
+  })
+
+  bench('lodash', () => {
+    isNilLodash(null);
+    isNilLodash(undefined);
+    isNilLodash(123);
+    isNilLodash([1, 2, 3]);
+  })
+});
