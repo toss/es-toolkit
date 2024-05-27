e8013806e653775ff5bd6f79ae8aea1d03383d28
raon0211 <raon0211@toss.im>
2024-05-27 12:59:33 +0900

    clamp

--- src/math/clamp.ts
+++ src/math/clamp.ts
@@ -5,8 +5,8 @@
  * If only one bound is provided, it returns the minimum of the value and the bound.
  *
  * @param {number} value - The number to clamp.
- * @param {number} bound1 - One of the bounds to clamp the number.
- * @param {number} [bound2] - The other bound to clamp the number. If not provided, `bound1` is used as the only bound.
+ * @param {number} minimum - The minimum bound to clamp the number.
+ * @param {number} maximum - The maximum bound to clamp the number.
  * @returns {number} The clamped number within the specified bounds.
  *
  * @example
@@ -16,6 +16,8 @@
  * const result3 = clamp(2, 5, 15); // result3 will be 5, as 2 is clamped to the lower bound 5
  * const result4 = clamp(20, 5, 15); // result4 will be 15, as 20 is clamped to the upper bound 15
  */
+export function clamp(value: number, maximum: number): number;
+export function clamp(value: number, minimum: number, maximum: number): number;
 export function clamp(value: number, bound1: number, bound2?: number): number {
   if (bound2 == null) {
     return Math.min(value, bound1);
