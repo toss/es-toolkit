848f2c66ff9528ba1c893206bb618fadec12bbe1
Jonghyeon Ko <jonghyeon@toss.im>
2024-06-03 22:30:32 +0900

    fix(package.json): Provide correct types for moduleResolution: node, node10, and node16 (#16)
    * fix: remove unnecessary exports overriding
    
    * Revert "fix: remove unnecessary exports overriding"
    
    This reverts commit f8534a14c18fc4e176b3587fac44797b7407402b.
    
    * chore(package.json): add types field in publishConfig.exports
    
    * build: Support moduleResolution: node10 and node16 (nodenext)
    
    * build: Support moduleResolution: node10 and node16 (nodenext)
    
    * build: Support moduleResolution: node10 and node16 (nodenext)
    
    * fix delay
    
    * Update .gitignore
    
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>
    
    * Update .scripts/postbuild.sh
    
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>
    
    ---------
    
    Co-authored-by: raon0211 <raon0211@toss.im>

---
+++ .scripts/postbuild.sh
@@ -0,0 +1,6 @@
+echo "export * from './dist/array';" > array.d.ts
+echo "export * from './dist/function';" > function.d.ts
+echo "export * from './dist/math';" > math.d.ts
+echo "export * from './dist/object';" > object.d.ts
+echo "export * from './dist/predicate';" > predicate.d.ts
+echo "export * from './dist/promise';" > promise.d.ts
