8d96b0ed872bb420bda9a6fb5885a294cf690a97
raon0211 <raon0211@toss.im>
2024-06-03 22:04:35 +0900

    build: Support moduleResolution: node10 and node16 (nodenext)

--- .scripts/postbuild.sh
+++ .scripts/postbuild.sh
@@ -4,4 +4,3 @@ echo "export * from './dist/math';" > math.d.ts
 echo "export * from './dist/object';" > object.d.ts
 echo "export * from './dist/predicate';" > predicate.d.ts
 echo "export * from './dist/promise';" > promise.d.ts
-echo "{\"type\":\"module\"}" > ./esm/package.json
