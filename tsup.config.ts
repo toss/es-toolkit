9bde10a5371d171b669d26c8f6ffa4663585f2b4
Jonghyeon Ko <jonghyeon@toss.im>
2024-06-04 09:56:07 +0900

    build: Support ESM type declarations & remove test files in dist (#21)
    * fix(*): migrate rollup to tsup, support esm correctly, remove *.spec.* for dist
    
    * chore: update
    
    * postbuild
    
    * Update .scripts/postbuild.sh
    
    * Update package.json
    
    Co-authored-by: Jonghyeon Ko <jonghyeon@toss.im>
    
    ---------
    
    Co-authored-by: raon0211 <raon0211@toss.im>

---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  format: ['cjs', 'esm'],
+  entry: ['src/*.ts', 'src/*/*.ts', '!**/*.{spec,test,test-d}.*'],
+  sourcemap: true,
+  dts: true,
+  clean: true,
+})
