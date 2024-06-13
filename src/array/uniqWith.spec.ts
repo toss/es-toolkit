360e438b1f37dda5380992b71b8be9a6d5457458
Sojin Park <raon0211@gmail.com>
2024-06-13 10:40:14 +0900

    Update src/array/uniqWith.spec.ts

--- src/array/uniqWith.spec.ts
+++ src/array/uniqWith.spec.ts
@@ -12,7 +12,6 @@ describe('uniqWith', () => {
         (a, b) => a.x === b.x
       )
     ).toEqual([{ x: 1, y: 2 }]);
-  });
     expect(uniqWith([1.2, 1.5, 2.1, 3.2, 5.7, 5.3, 7.19], (a, b) => Math.abs(a - b) < 1)).toEqual([
       1.2, 3.2, 5.7, 7.19,
     ]);
