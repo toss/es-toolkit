75df723ca20456b9217254a52ed1fe0efe907f95
jang_yoonsu <shagrat8546@gmail.com>
2024-06-14 23:29:52 +0900

    refactor : Change the words min, max to minimum, maximum

--- src/math/random.ts
+++ src/math/random.ts
@@ -1,18 +1,18 @@
 /**
- * Generates a random floating-point number between min (inclusive) and max (exclusive).
- * If min is greater than max, the values are swapped.
+ * Generates a random floating-point number between minimum (inclusive) and maximum (exclusive).
+ * If min is greater than maximum, the values are swapped.
  *
- * @param {number} min - The lower bound (inclusive).
- * @param {number} max - The upper bound (exclusive).
- * @returns {number} A random integer between min (inclusive) and max (exclusive).
+ * @param {number} minimum - The lower bound (inclusive).
+ * @param {number} maximum - The upper bound (exclusive).
+ * @returns {number} A random integer between minimum (inclusive) and maximum (exclusive).
  *
  * @example
  * const result = random(0, 5); // result will be a random floating-point number between 0 (inclusive) and 5 (exclusive)
  * const result2 = random(5, 0); // result2 will also be a random floating-point number between 0 (inclusive) and 5 (exclusive)
  */
-export function random(min: number, max: number): number {
-  if (min > max) {
-    [min, max] = [max, min];
+export function random(minimum: number, maximum: number): number {
+  if (minimum > maximum) {
+    [minimum, maximum] = [maximum, minimum];
   }
-  return Math.random() * (max - min) + min;
+  return Math.random() * (maximum - minimum) + minimum;
 }
