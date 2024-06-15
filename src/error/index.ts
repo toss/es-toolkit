c4bbfc89b490caa0bf1b698bea3aba02550e51b8
Hanna922 <hanna.urssu@gmail.com>
2024-06-15 14:18:46 +0900

    feat: separate error file

---
+++ src/error/index.ts
@@ -0,0 +1,1 @@
+export { AbortError } from './AbortError';
--- src/promise/delay.ts
+++ src/promise/delay.ts
@@ -1,3 +1,5 @@
+import { AbortError } from '../error/AbortError';
+
 interface DelayOptions {
   signal?: AbortSignal;
 }
@@ -37,7 +39,7 @@ interface DelayOptions {
 export function delay(ms: number, { signal }: DelayOptions = {}): Promise<void> {
   return new Promise((resolve, reject) => {
     const abortError = () => {
-      reject(new Error('The operation was aborted'));
+      reject(new AbortError());
     };
 
     const abortHandler = () => {
