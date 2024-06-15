31be06ffc4bddf26f97e42a4251379489b9e9cab
changwoolab <cwyoo01@naver.com>
2024-06-15 20:58:30 +0900

    perf: make more performant when handling large arrays

--- src/array/intersection.spec.ts
+++ src/array/intersection.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { intersection } from './intersection';
+import { randomInt } from 'crypto';
 
 describe('intersection', () => {
   it('should return the intersection of two arrays', () => {
@@ -7,5 +8,13 @@ describe('intersection', () => {
     expect(intersection([1, 2], [3, 1])).toEqual([1]);
     expect(intersection([1, 2], [3, 4])).toEqual([]);
     expect(intersection([], [1, 2])).toEqual([]);
+  });
+
+  it('handles large arrays', () => {
+    const array1 = Array.from({ length: 130 }, () => randomInt(0, 1000));
+    expect(intersection(array1, array1)).toEqual(array1);
+
+    const array2 = Array.from({ length: 130 }, () => randomInt(0, 1000));
+    expect(intersection(array1, array2)).toEqual(array1.filter(x => array2.includes(x)));
   });
 });
--- src/array/intersection.ts
+++ src/array/intersection.ts
@@ -1,3 +1,5 @@
+import { FastMap } from '../utils/FastMap';
+
 /**
  * Returns the intersection of two arrays.
  *
@@ -16,9 +18,34 @@
  * // result will be [3, 4, 5] since these elements are in both arrays.
  */
 export function intersection<T>(firstArr: readonly T[], secondArr: readonly T[]): T[] {
+  if (firstArr.length < 120) {
+    return intersectionSmallArrays(firstArr, secondArr);
+  }
+
+  return intersectionLargeArrays(firstArr, secondArr);
+}
+
+function intersectionSmallArrays<T>(firstArr: readonly T[], secondArr: readonly T[]): T[] {
   const secondSet = new Set(secondArr);
 
   return firstArr.filter(item => {
     return secondSet.has(item);
   });
+}
+
+function intersectionLargeArrays<T>(firstArr: readonly T[], secondArr: readonly T[]): T[] {
+  const map = new FastMap();
+
+  for (const item of secondArr) {
+    map.set(item, true);
+  }
+
+  const result = [];
+  for (const item of firstArr) {
+    if (map.has(item)) {
+      result.push(item);
+      map.set(item, true);
+    }
+  }
+  return result;
 }
