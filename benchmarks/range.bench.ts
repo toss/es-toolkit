1c3c4aab84f6f4ca0c73b22b5de4969918a9a6be
WooWan <woohobi@gmail.com>
2024-06-19 00:22:46 +0900

    chore(range): update bench name

--- benchmarks/range.bench.ts
+++ benchmarks/range.bench.ts
@@ -3,11 +3,11 @@ import { range as rangeToolkit } from 'es-toolkit';
 import { range as rangeLodash } from 'lodash';
 
 describe('range', () => {
-  bench('es-toolkit', () => {
+  bench('es-toolkit/range', () => {
     rangeToolkit(0, 100, 1);
   });
 
-  bench('lodash', () => {
+  bench('lodash/range', () => {
     rangeLodash(0, 100, 1);
   });
 });
