6ace3f6eee8cb91058eae65a43ad8686f6dbf224
Sojin Park <raon0211@gmail.com>
2024-06-30 13:23:06 +0900

    chore: Add publish for jsr step

--- README.md
+++ README.md
@@ -12,6 +12,26 @@ es-toolkit is a state-of-the-art, high-performance JavaScript utility library wi
 - es-toolkit includes built-in TypeScript support, with straightforward yet robust types. It also provides useful type guards such as [isNotNil](https://es-toolkit.slash.page/reference/predicate/isNotNil.html).
 - es-toolkit is battle-tested with 100% test coverage, ensuring reliability and robustness.
 
+## Examples
+
+```tsx
+// import from '@es-toolkit/es-toolkit' in jsr.
+import { debounce, chunk } from 'es-toolkit';
+
+const debouncedLog = debounce(message => {
+  console.log(message);
+}, 300);
+
+// This call will be debounced
+debouncedLog('Hello, world!');
+
+const array = [1, 2, 3, 4, 5, 6];
+const chunkedArray = chunk(array, 2);
+
+console.log(chunkedArray);
+// Output: [[1, 2], [3, 4], [5, 6]]
+```
+
 ## Contributing
 
 We welcome contribution from everyone in the community. Read below for detailed contribution guide.
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+/**
+ * es-toolkit: A modern JavaScript utility library that's 2-3 times faster and up to 97% smaller—a major upgrade to lodash.
+ * ====================================
+ * es-toolkit is a state-of-the-art, high-performance JavaScript utility library
+ * with a small bundle size and strong type annotations.
+ *
+ * Here are some of the features es-toolkit offers:
+ *
+ * - **Array**: Utilities for array manipulation, such as [uniq](https://es-toolkit.slash.page/reference/array/uniq.html) and [difference](https://es-toolkit.slash.page/reference/array/difference.html).
+ * - **Function**: Tools for controlling function execution, including [debounce](https://es-toolkit.slash.page/reference/function/debounce.html) and [throttle](https://es-toolkit.slash.page/reference/function/throttle.html).
+ * - **Math**: Numerical utilities like [sum](https://es-toolkit.slash.page/reference/math/sum.html) and [round](https://es-toolkit.slash.page/reference/math/round.html).
+ * - **Object**: Tools for manipulating JavaScript objects, such as [pick](https://es-toolkit.slash.page/reference/object/pick.html) and [omit](https://es-toolkit.slash.page/reference/object/omit.html).
+ * - **Predicate**: Type guard functions like [isNotNil](https://es-toolkit.slash.page/reference/predicate/isNotNil.html).
+ * - **Promise**: Asynchronous utilities like [delay](https://es-toolkit.slash.page/reference/promise/delay.html).
+ *
+ * If you want to know more about the project, please take a look at the
+ * following resources:
+ *
+ * - [GitHub](https://github.com/toss/es-toolkit)
+ * - [Documentation](https://es-toolkit.slash.page)
+ *
+ * @module
+ */
 export * from './array';
 export * from './error';
 export * from './function';
