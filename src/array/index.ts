2448aeb773c6cd266cb5f560e2a871391cf6808d
LeeSeungHun <dos07008@naver.com>
2024-07-09 21:12:26 +0900

    feat(tail): add tail function

--- src/array/index.ts
+++ src/array/index.ts
@@ -42,3 +42,4 @@ export { zipObject } from './zipObject.ts';
 export { zipWith } from './zipWith.ts';
 export { without } from './without.ts';
 export { head } from './head.ts';
+export { tail } from './tail.ts';
