e27d206be0ed39f64f01655f27a5c904ef9df4e7
haejunejung <jhj9422@unist.ac.kr>
2024-07-10 14:42:08 +0900

    feat(snakeCase): Add caseSplitPattern RegExp const

---
+++ src/constants/caseSplitPattern.ts
@@ -0,0 +1,18 @@
+/**
+ * Regular expression pattern to split strings into words for various case conversions
+ *
+ * This pattern matchs sequences of characters in a string, considering the following case:
+ * - Sequences of two or more uppercase letters followed by an uppercase letter and lowercase letters or digits (for acronyms)
+ * - Sequences of one uppercase letter optionally followed by lowercase letters and digits
+ * - Single uppercase letters
+ * - Sequences of digis
+ *
+ * The resulting match can be used to convert camelCase, snake_case, kebab-case, and other mixed formats into
+ * a consistent format like snake case.
+ *
+ * @example
+ * const matches = 'camelCaseHTTPRequest'.match(CASE_SPLIT_PATTERN);
+ * // matchs: ['camel', 'Case', 'HTTP', 'Request']
+ */
+
+export const CASE_SPLIT_PATTERN = /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g;
