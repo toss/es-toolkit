9cf10c0e85c136d9248453786d890db251440c86
haejunejung <jhj9422@unist.ac.kr>
2024-07-10 14:43:16 +0900

    feat(snakeCase): Add snakeCase function

---
+++ src/string/snakeCase.ts
@@ -0,0 +1,22 @@
+import { CASE_SPLIT_PATTERN } from '../constants';
+
+/**
+ * Converts a string to snake case.
+ *
+ * Snake case is the naming convention in which each space is replaced with 
+ * an underscore(_) character and words are written in lowercase.
+ *
+ * @param {string} str - The string that is to be changed to snake case.
+ * @returns {string} - The converted string to snake case.
+ *
+ * @example
+ * const convertedStr1 = snakeCase('camelCase') // returns 'camel_case'
+ * const convertedStr2 = snakeCase('some whitespace') // returns 'some_whitespace'
+ * const convertedStr3 = snakeCase('hyphen-text') // returns 'hyphen_text'
+ * const convertedStr4 = snakeCase('HTTPRequest') // returns 'http_request'
+ */
+
+export const snakeCase = (str: string): string => {
+  const splitWords = str.match(CASE_SPLIT_PATTERN) || [];
+  return splitWords.map(word => word.toLowerCase()).join('_');
+};
