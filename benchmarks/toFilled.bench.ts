ee615af6c17ec7518db70cb5516e79b65e68dd47
wondonghwi <ehdehd1289@naver.com>
2024-07-11 00:58:04 +0900

    feat: add bench for toFilled

---
+++ benchmarks/toFilled.bench.ts
@@ -0,0 +1,23 @@
+import { bench, describe } from 'vitest';
+import { fill as fillLodash } from 'lodash';
+import { fill as toFilled } from 'es-toolkit';
+
+describe('fill function performance comparison', () => {
+  bench('toFilled', () => {
+    toFilled([1, 2, 3, 4, 5], '*');
+  });
+
+  bench('lodash/fill', () => {
+    fillLodash([1, 2, 3, 4, 5], '*');
+  });
+});
+
+describe('fill function performance with custom start and end', () => {
+  bench('toFilled', () => {
+    toFilled([1, 2, 3, 4, 5], '*', 1, 3);
+  });
+
+  bench('lodash/fill', () => {
+    fillLodash([1, 2, 3, 4, 5], '*', 1, 3);
+  });
+});
