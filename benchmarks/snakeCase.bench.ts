2709eafa627e5c526f46b5676d610c6d3067f216
changwoolab <cwyoo01@naver.com>
2024-07-13 15:03:20 +0900

    test: add bench to make es-toolkit faster with long string

--- benchmarks/snakeCase.bench.ts
+++ benchmarks/snakeCase.bench.ts
@@ -3,13 +3,28 @@ import { snakeCase as snakeCaseToolkit } from 'es-toolkit';
 import { snakeCase as snakeCaseLodash } from 'lodash';
 
 describe('snakeCase', () => {
-  bench('es-toolkit/snakeCase', () => {
-    const str = 'camelCase';
-    snakeCaseToolkit(str);
+  describe('short string', () => {
+    bench('es-toolkit/snakeCase', () => {
+      const str = 'camelCase';
+      snakeCaseToolkit(str);
+    });
+
+    bench('lodash/snakeCase', () => {
+      const str = 'camelCase';
+      snakeCaseLodash(str);
+    });
   });
 
-  bench('lodash/snakeCase', () => {
-    const str = 'camelCase';
-    snakeCaseLodash(str);
+  describe('long string', () => {
+    const LONG_STRING = 'thisIsAVeryLongStringThatContainsMultipleWords'.repeat(50);
+    bench('es-toolkit/snakeCase', () => {
+      const str = LONG_STRING;
+      snakeCaseToolkit(str);
+    });
+
+    bench('lodash/snakeCase', () => {
+      const str = LONG_STRING;
+      snakeCaseLodash(str);
+    });
   });
 });
