f448f2b8502e9d1ed7797d0a5807e854ea203f86
changwoolab <cwyoo01@naver.com>
2024-07-13 16:55:47 +0900

    test: adds bench for long string

--- benchmarks/kebabCase.bench.ts
+++ benchmarks/kebabCase.bench.ts
@@ -3,13 +3,27 @@ import { kebabCase as kebabCaseToolkit } from 'es-toolkit';
 import { kebabCase as kebabCaseLodash } from 'lodash';
 
 describe('kebabCase', () => {
-  bench('es-toolkit/kebabCase', () => {
-    const str = 'camelCase';
-    kebabCaseToolkit(str);
+  describe('short string', () => {
+    bench('es-toolkit/kebabCase', () => {
+      const str = 'camelCase';
+      kebabCaseToolkit(str);
+    });
+
+    bench('lodash/kebabCase', () => {
+      const str = 'camelCase';
+      kebabCaseLodash(str);
+    });
   });
 
-  bench('lodash/kebabCase', () => {
-    const str = 'camelCase';
-    kebabCaseLodash(str);
+  describe('long string', () => {
+    const LONG_STR = 'camelCaseLongString'.repeat(100);
+
+    bench('es-toolkit/kebabCase', () => {
+      kebabCaseToolkit(LONG_STR);
+    });
+
+    bench('lodash/kebabCase', () => {
+      kebabCaseLodash(LONG_STR);
+    });
   });
 });
