c6b22265d992582f8e2ecd94eb3599f899cc7cef
changwoolab <cwyoo01@naver.com>
2024-07-13 22:36:49 +0900

    test: add bench for capitalize

--- benchmarks/capitalize.bench.ts
+++ benchmarks/capitalize.bench.ts
@@ -3,13 +3,26 @@ import { capitalize as capitalizeToolkit } from 'es-toolkit';
 import { capitalize as capitalizeLodash } from 'lodash';
 
 describe('capitalize', () => {
-  bench('es-toolkit/capitalize', () => {
-    const str = 'camelCase';
-    capitalizeToolkit(str);
+  describe('short string', () => {
+    bench('es-toolkit/capitalize', () => {
+      const str = 'camelCase';
+      capitalizeToolkit(str);
+    });
+
+    bench('lodash/capitalize', () => {
+      const str = 'camelCase';
+      capitalizeLodash(str);
+    });
   });
 
-  bench('lodash/capitalize', () => {
-    const str = 'camelCase';
-    capitalizeLodash(str);
+  describe('long string', () => {
+    const LONG_STR = 'camelCaseLongString'.repeat(100);
+    bench('es-toolkit/capitalize', () => {
+      capitalizeToolkit(LONG_STR);
+    });
+
+    bench('lodash/capitalize', () => {
+      capitalizeLodash(LONG_STR);
+    });
   });
 });
