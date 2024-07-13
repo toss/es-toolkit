5692887c323f4c7a967f44c2840d33bba05d6ddb
changwoolab <cwyoo01@naver.com>
2024-07-13 17:02:40 +0900

    test: add bench for long string

--- benchmarks/lowerCase.bench.ts
+++ benchmarks/lowerCase.bench.ts
@@ -3,13 +3,26 @@ import { lowerCase as lowerCaseToolkit } from 'es-toolkit';
 import { lowerCase as lowerCaseLodash } from 'lodash';
 
 describe('lowerCase', () => {
-  bench('es-toolkit/lowerCase', () => {
-    const str = 'camelCase';
-    lowerCaseToolkit(str);
+  describe('short string', () => {
+    bench('es-toolkit/lowerCase', () => {
+      const str = 'camelCase';
+      lowerCaseToolkit(str);
+    });
+
+    bench('lodash/lowerCase', () => {
+      const str = 'camelCase';
+      lowerCaseLodash(str);
+    });
   });
 
-  bench('lodash/lowerCase', () => {
-    const str = 'camelCase';
-    lowerCaseLodash(str);
+  describe('long string', () => {
+    const LONG_STR = 'camelCase'.repeat(100);
+    bench('es-toolkit/lowerCase', () => {
+      lowerCaseToolkit(LONG_STR);
+    });
+
+    bench('lodash/lowerCase', () => {
+      lowerCaseLodash(LONG_STR);
+    });
   });
 });
