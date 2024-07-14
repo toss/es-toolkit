e491e76c5c149a45682ab874ac72d90d85441456
Denovo <vo0v0000@naver.com>
2024-07-15 08:08:34 +0900

    chore: add benchmark of cloneDeep

---
+++ benchmarks/cloneDeep.bench.ts
@@ -0,0 +1,26 @@
+import { bench, describe } from 'vitest';
+import { cloneDeep } from 'es-toolkit';
+import { cloneDeep as lodashCloneDeep } from 'lodash';
+
+const obj = {
+  number: 29,
+  string: 'es-toolkit',
+  boolean: true,
+  array: [1, 2, 3],
+  object: { a: 1, b: 'es-toolkit' },
+  date: new Date(),
+  regex: /abc/g,
+  nested: { a: [1, 2, 3], b: { c: 'es-toolkit' }, d: new Date() },
+};
+
+describe('clone', () => {
+  bench('es-toolkit/clone', () => {
+    cloneDeep(obj);
+  });
+  bench('lodash/cloneDeep', () => {
+    lodashCloneDeep(obj);
+  });
+  bench('JSON.parse', () => {
+    JSON.parse(JSON.stringify(obj));
+  });
+});
