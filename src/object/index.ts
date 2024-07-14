a6d42c5c913482b19859783a4e64f3d2a3ae4e96
Denovo <vo0v0000@naver.com>
2024-07-15 08:20:53 +0900

    feat: add index

--- src/object/index.ts
+++ src/object/index.ts
@@ -4,3 +4,4 @@ export { pick } from './pick.ts';
 export { pickBy } from './pickBy.ts';
 export { invert } from './invert.ts';
 export { clone } from './clone.ts';
+export { cloneDeep } from './cloneDeep.ts';
