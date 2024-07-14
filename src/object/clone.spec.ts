128147b1081e99ff5b082a70a072bbcc16e403dd
Jun <juno.lee@toss.im>
2024-07-14 17:46:25 +0900

    feat(clone): Add clone (#155)
    * feat: add clone(shallow copy) implementaion and test codes
    
    * chore: add benchamark of clone
    
    * docs: add docs of clone
    
    * chore: bench number
    
    * Update docs/ko/reference/object/clone.md
    
    ---------
    
    Co-authored-by: Sojin Park <raon0211@toss.im>

---
+++ src/object/clone.spec.ts
@@ -0,0 +1,64 @@
+import { describe, it, expect } from 'vitest';
+import { clone } from './clone';
+
+describe('clone', () => {
+  it('should return primitive values as is', () => {
+    const symbol = Symbol('symbol');
+
+    expect(clone(42)).toBe(42);
+    expect(clone('es-toolkit')).toBe('es-toolkit');
+    expect(clone(symbol)).toBe(symbol);
+    expect(clone(true)).toBe(true);
+    expect(clone(null)).toBe(null);
+    expect(clone(undefined)).toBe(undefined);
+  });
+
+  it('should clone arrays', () => {
+    const arr = [1, 2, 3];
+    const clonedArr = clone(arr);
+
+    expect(clonedArr).toEqual(arr);
+    expect(clonedArr).not.toBe(arr);
+  });
+
+  it('should clone objects', () => {
+    const obj = { a: 1, b: 'es-toolkit' };
+    const clonedObj = clone(obj);
+
+    expect(clonedObj).toEqual(obj);
+    expect(clonedObj).not.toBe(obj);
+  });
+
+  it('should clone dates', () => {
+    const date = new Date();
+    const clonedDate = clone(date);
+
+    expect(clonedDate).toEqual(date);
+    expect(clonedDate).not.toBe(date);
+  });
+
+  it('should clone regular expressions', () => {
+    const regex = /abc/g;
+    const clonedRegex = clone(regex);
+
+    expect(clonedRegex).toEqual(regex);
+    expect(clonedRegex).not.toBe(regex);
+  });
+
+  it('should shallow clone nested objects', () => {
+    const nestedObj = { a: [1, 2, 3], b: { c: 'es-toolkit' }, d: new Date() };
+    const clonedNestedObj = clone(nestedObj);
+
+    expect(clonedNestedObj).toEqual(nestedObj);
+    expect(clonedNestedObj).not.toBe(nestedObj);
+    expect(clonedNestedObj.a).toEqual(nestedObj.a);
+    expect(clonedNestedObj.a[2]).toEqual(nestedObj.a[2]);
+  });
+
+  it('should return functions as is', () => {
+    const func = () => {};
+    const clonedFunc = clone(func);
+
+    expect(clonedFunc).toBe(func);
+  });
+});
