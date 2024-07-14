ec5746ac08e9e902bc981d616879aee8182eeffd
Sojin Park <raon0211@gmail.com>
2024-07-14 18:08:02 +0900

    Update negate.spec.ts

--- src/function/negate.spec.ts
+++ src/function/negate.spec.ts
@@ -2,22 +2,9 @@ import { describe, it, expect } from 'vitest';
 import { negate } from './negate';
 
 describe('negate', () => {
-    it('should be true', () => {
-        expect(negate(() => undefined)()).toBe(true);
-        expect(negate(() => null)()).toBe(true);
-        expect(negate(() => +0)()).toBe(true);
-        expect(negate(() => -0)()).toBe(true);
-        expect(negate(() => NaN)()).toBe(true);
-        expect(negate(() => 0)()).toBe(true);
-        expect(negate(() => "")()).toBe(true);
-        expect(negate(() => false)()).toBe(true);
-    });
-
-    it('should be false', () => {
+    it('should negate the given predicate function', () => {
+        expect(typeof negate(() => true)).toBe('function');
         expect(negate(() => true)()).toBe(false);
-        expect(negate(() => +1)()).toBe(false);
-        expect(negate(() => -1)()).toBe(false);
-        expect(negate(() => [])()).toBe(false);
-        expect(negate(() => "string")()).toBe(false);
+        expect(negate(() => false)()).toBe(true);
     });
 });
