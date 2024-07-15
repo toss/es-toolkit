fec678cf34c2387e93acd934134ec7eaff7d766a
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-07-15 22:43:15 +0900

    feat(flatMap): add flatMap function (#209)
    * feat(flatMap): add flatMap function
    
    * docs: fix docs
    
    * Update src/array/flatMap.ts
    
    ---------
    
    Co-authored-by: Sojin Park <raon0211@gmail.com>

---
+++ src/array/flatMap.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it } from 'vitest';
+import { flatMap } from './flatMap';
+
+describe('flatMap', () => {
+  const originArr = [1, 2, 3];
+
+  it('should map and flatten array of numbers with default depth', () => {
+    const iteratee = (item: number) => [item, item];
+    const expectedArr = [1, 1, 2, 2, 3, 3];
+
+    expect(flatMap(originArr, iteratee)).toEqual(expectedArr);
+  });
+
+  it('should map and flatten array of numbers with specified depth', () => {
+    const iteratee = (item: number) => [[[item, item]]];
+
+    const expectedArr1 = [[[1, 1]], [[2, 2]], [[3, 3]]];
+    expect(flatMap(originArr, iteratee, 1)).toEqual(expectedArr1);
+
+    const expectedArr2 = [
+      [1, 1],
+      [2, 2],
+      [3, 3],
+    ];
+    expect(flatMap(originArr, iteratee, 2)).toEqual(expectedArr2);
+
+    const expectedArr3 = [1, 1, 2, 2, 3, 3];
+    expect(flatMap(originArr, iteratee, 3)).toEqual(expectedArr3);
+  });
+
+  it('should handle empty array', () => {
+    const emptyArr: number[] = [];
+    const result = flatMap(emptyArr, item => [item, item]);
+    expect(result).toEqual([]);
+  });
+});
