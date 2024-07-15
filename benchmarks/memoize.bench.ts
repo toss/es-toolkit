c538aa1c3802e0f1c4e9b70453219541102ad775
Denovo <vo0v0000@naver.com>
2024-07-15 14:25:20 +0900

    chore: add benchmark of memoize

---
+++ benchmarks/memoize.bench.ts
@@ -0,0 +1,22 @@
+import { bench, describe } from 'vitest';
+import { memoize } from 'es-toolkit';
+import { memoize as lodashMemoize } from 'lodash';
+
+describe('memoize', () => {
+  const object = { a: 1, b: 2, c: 3 };
+  const other = { d: 4, e: 5, f: 6 };
+  const values = (args: object) => {
+    return Object.values(args);
+  };
+  bench('es-toolkit/memoize', () => {
+    const memoized = memoize(values);
+    memoized(object);
+    memoized(other);
+  });
+
+  bench('lodash/memoize', () => {
+    const memoized = lodashMemoize(values);
+    memoized(object);
+    memoized(other);
+  });
+});
