50f08d16bf143f237df4df4de31b70956b11fb50
changwoolab <cwyoo01@naver.com>
2024-07-16 10:43:43 +0900

    test: add tests for drop

--- src/array/drop.spec.ts
+++ src/array/drop.spec.ts
@@ -6,4 +6,17 @@ describe('drop', () => {
     expect(drop([1.2, 2.3, 3.4], 1)).toEqual([2.3, 3.4]);
     expect(drop(['a', 'b', 'c', 'd'], 2)).toEqual(['c', 'd']);
   });
+
+  it('should return all elements if n < 1', () => {
+    expect(drop([1.2, 2.3, 3.4], 0)).toEqual([1.2, 2.3, 3.4]);
+    expect(drop([1.2, 2.3, 3.4], -1)).toEqual([1.2, 2.3, 3.4]);
+  });
+
+  it('should coerce n to an integer', () => {
+    expect(drop([1.2, 2.3, 3.4], 1.5)).toEqual([2.3, 3.4]);
+  });
+
+  it('should return empty array if n > arr.length', () => {
+    expect(drop([1.2, 2.3, 3.4], 4)).toEqual([]);
+  });
 });
