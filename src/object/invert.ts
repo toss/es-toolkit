3dd8009b0276bf1ca1683cb2ba89174a33123ef6
changwoolab <cwyoo01@naver.com>
2024-07-16 13:24:44 +0900

    fix: invert not to invert inherited properties

--- src/object/invert.ts
+++ src/object/invert.ts
@@ -19,9 +19,9 @@
 export function invert<K extends PropertyKey, V extends PropertyKey>(obj: Record<K, V>): { [key in V]: K } {
   const result = {} as { [key in V]: K };
 
-  for (const key in obj) {
+  for (const key of Object.keys(obj)) {
     const value = obj[key as K] as V;
-    result[value] = key;
+    result[value] = key as K;
   }
 
   return result;
