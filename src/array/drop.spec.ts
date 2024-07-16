4877558eb0d789d2d4fc07482ee52b7fbc052c2c
changwoolab <cwyoo01@naver.com>
2024-07-16 10:51:20 +0900

    test: fixed phrase

--- src/array/drop.spec.ts
+++ src/array/drop.spec.ts
@@ -7,16 +7,16 @@ describe('drop', () => {
     expect(drop(['a', 'b', 'c', 'd'], 2)).toEqual(['c', 'd']);
   });
 
-  it('should return all elements if n < 1', () => {
+  it('should return all elements if itemsCount < 1', () => {
     expect(drop([1.2, 2.3, 3.4], 0)).toEqual([1.2, 2.3, 3.4]);
     expect(drop([1.2, 2.3, 3.4], -1)).toEqual([1.2, 2.3, 3.4]);
   });
 
-  it('should coerce n to an integer', () => {
+  it('should coerce itemsCount to an integer', () => {
     expect(drop([1.2, 2.3, 3.4], 1.5)).toEqual([2.3, 3.4]);
   });
 
-  it('should return empty array if n >= arr.length', () => {
+  it('should return empty array if itemsCount >= arr.length', () => {
     expect(drop([1.2, 2.3, 3.4], 4)).toEqual([]);
   });
 });
