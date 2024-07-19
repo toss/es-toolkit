6c632b1ac6920638cb91c3dcd4bc5b0af2605ad5
sunrabbit123 <qudwls185@naver.com>
2024-07-19 01:18:07 -0700

    test(isLength, compat): add test case link
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- src/compat/predicate/isLength.spec.ts
+++ src/compat/predicate/isLength.spec.ts
@@ -1,6 +1,9 @@
 import { isLength } from '../index';
 import { describe, expect, it } from 'vitest';
 
+/**
+ * @see https://github.com/lodash/lodash/blob/6a2cc1dfcf7634fea70d1bc5bd22db453df67b42/test/isLength.spec.js#L1
+ */
 describe('isLength', () => {
   it('should return `true` for lengths', () => {
     const values = [0, 3, Number.MAX_SAFE_INTEGER];
