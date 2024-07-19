95a6a6b60b463a5e887d23890e485587bc562e4c
seungro <seungro@toss.im>
2024-07-19 12:07:38 +0900

    chore. add pick to index

--- src/compat/index.ts
+++ src/compat/index.ts
@@ -32,6 +32,7 @@ export { head as first } from '../index.ts';
 
 export { get } from './object/get.ts';
 export { set } from './object/set.ts';
+export { pick } from './object/pick.ts';
 
 export { startsWith } from './string/startsWith.ts';
 export { endsWith } from './string/endsWith.ts';
