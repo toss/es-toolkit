ada9c78d3cfcd48820e5c32cc6fee651afbaedd7
haejunejung <jhj9422@unist.ac.kr>
2024-07-20 22:50:47 +0900

    fix(isUndefined): fix typo

--- benchmarks/performance/isUndefined.bench.ts
+++ benchmarks/performance/isUndefined.bench.ts
@@ -1,13 +1,13 @@
 import { bench, describe } from 'vitest';
-import { isUndefined as isUndefinedToolKit } from 'es-toolkit';
+import { isUndefined as isUndefinedToolkit } from 'es-toolkit';
 import { isUndefined as isUndefinedLodash } from 'lodash';
 
 describe('isUndefined', () => {
   bench('es-toolkit/isUndefined', () => {
-    isUndefinedToolKit(undefined);
-    isUndefinedToolKit(null);
-    isUndefinedToolKit('');
-    isUndefinedToolKit(123);
+    isUndefinedToolkit(undefined);
+    isUndefinedToolkit(null);
+    isUndefinedToolkit('');
+    isUndefinedToolkit(123);
   });
   bench('lodash/isUndefined', () => {
     isUndefinedLodash(undefined);
