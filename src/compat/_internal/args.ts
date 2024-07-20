c677e0fc4bac012bcaa0b0d64e23d54fb341b391
dayongkr <dayongkr@gmail.com>
2024-07-20 21:04:28 +0900

    add toArgs testcase and remove a unusable expression

--- src/compat/_internal/args.ts
+++ src/compat/_internal/args.ts
@@ -1,20 +1,3 @@
+import { toArgs } from './toArgs';
+
 export const args = toArgs([1, 2, 3]);
-
-/**
- * Converts an array to an `arguments` object.
- *
- * @param {unknown[]} array - The array to convert.
- * @returns {IArguments} - The `arguments` object.
- *
- * @example
- * toArgs([1, 2, 3]); // { '0': 1, '1': 2, '2': 3 } as IArguments
- */
-
-function toArgs(array: unknown[]): IArguments {
-  // eslint-disable-next-line prefer-spread
-  return (function (...args) {
-    args;
-    // eslint-disable-next-line prefer-rest-params
-    return arguments;
-  })(...array);
-}
---
+++ src/compat/_internal/toArgs.ts
@@ -0,0 +1,17 @@
+/**
+ * Converts an array to an `arguments` object.
+ *
+ * @param {unknown[]} array - The array to convert.
+ * @returns {IArguments} - The `arguments` object.
+ *
+ * @example
+ * toArgs([1, 2, 3]); // { '0': 1, '1': 2, '2': 3 } as IArguments
+ */
+
+export function toArgs(array: unknown[]): IArguments {
+  // eslint-disable-next-line prefer-spread, @typescript-eslint/no-unused-vars
+  return (function (..._args) {
+    // eslint-disable-next-line prefer-rest-params
+    return arguments;
+  })(...array);
+}
