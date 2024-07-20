85339379e751b11c7667db6c2cf8566ec46cf9f4
haejunejung <jhj9422@unist.ac.kr>
2024-07-20 22:41:16 +0900

    feat(isUndefined): Add compat's isUndefined test code

---
+++ src/compat/predicate/isUndefined.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from 'vitest';
+import { isUndefined } from '../../predicate';
+import { falsey } from '../_internal/falsey';
+
+describe('isUndefined', () => {
+  it('should return `true` for `undefined` values', () => {
+    expect(isUndefined(undefined)).toBe(true);
+  });
+
+  it('should return `false` for non `undefined` values', () => {
+    const expected = falsey.map(value => value === undefined);
+    const actual = falsey.map((value, index) => (index ? isUndefined(value) : isUndefined(undefined)));
+    expect(actual).toEqual(expected);
+
+    (function () {
+      expect(isUndefined(arguments)).toBe(false);
+    })(1, 2, 3);
+    expect(isUndefined([1, 2, 3])).toBe(false);
+    expect(isUndefined(true)).toBe(false);
+    expect(isUndefined(new Date())).toBe(false);
+    expect(isUndefined(new Error())).toBe(false);
+    expect(isUndefined(Array.prototype.slice)).toBe(false);
+    expect(isUndefined({ a: 1 })).toBe(false);
+    expect(isUndefined(1)).toBe(false);
+    expect(isUndefined(/x/)).toBe(false);
+    expect(isUndefined('a')).toBe(false);
+    expect(isUndefined(Symbol('a'))).toBe(false);
+  });
+});
