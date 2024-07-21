e0469bf39445e1bfaf54fc1d21f542d057a65fd9
Dayong Lee <dayongkr@gmail.com>
2024-07-21 10:58:40 +0900

    feat(fill): Add compatibility with lodash (#259)
    * feat(fill): Add compatibility with lodash
    
    * Change docs and export fill

--- src/compat/index.ts
+++ src/compat/index.ts
@@ -27,6 +27,7 @@ export * from '../index.ts';
 export { chunk } from './array/chunk.ts';
 export { concat } from './array/concat.ts';
 export { difference } from './array/difference.ts';
+export { fill } from './array/fill.ts';
 export { zipObjectDeep } from './array/zipObjectDeep.ts';
 export { head as first } from '../index.ts';
 
