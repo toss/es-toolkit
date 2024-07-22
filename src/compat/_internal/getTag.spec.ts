a8a760c390a9f2262da62b6ac99b333f51641ed8
dayongkr <dayongkr@gmail.com>
2024-07-22 15:12:31 +0900

    Add isArguments

---
+++ src/compat/_internal/getTag.spec.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest';
+import { getTag } from './getTag';
+
+describe('getTag function', () => {
+  it('should return the tag of the value', () => {
+    expect(getTag(null)).toBe('[object Null]');
+    expect(getTag(undefined)).toBe('[object Undefined]');
+    expect(getTag(1)).toBe('[object Number]');
+    expect(getTag('')).toBe('[object String]');
+    expect(getTag(true)).toBe('[object Boolean]');
+    expect(getTag(Symbol())).toBe('[object Symbol]');
+    expect(getTag([])).toBe('[object Array]');
+    expect(getTag({})).toBe('[object Object]');
+    expect(getTag(() => {})).toBe('[object Function]');
+    expect(getTag(new Date())).toBe('[object Date]');
+    expect(getTag(/./)).toBe('[object RegExp]');
+  });
+
+  it('should return the tag of the custom object', () => {
+    class Custom {}
+    expect(getTag(new Custom())).toBe('[object Object]');
+  });
+});
