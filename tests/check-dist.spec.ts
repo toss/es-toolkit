7b2aa1adfc218dcd8160a28df9482854d8dcb2da
Florens Verschelde <243601+fvsch@users.noreply.github.com>
2024-07-24 17:37:53 +0200

    chore: use rollup instead of tsup for a cleaner dist (#255)
    * update tsup to 8.2.0 (no change to dist)
    
    * add rollup config to generate dist
    
    * fix browser build config
    
    * browser umd bundle sourcemap should be published
    
    * browser bundle is iife, not umd, despite folder name
    
    * rollup: don't use manualChunks to keep the config a tad simpler
    
    * prepack: use rollup instead of tsup
    
    * remove browser entrypoint from dist
    
    * remove comments in .js and .mjs outputs
    
    * remove src/browser.ts, use equivalent Rollup config
    
    * Restore original .npmignore
    
    * rollup config: rename umdBuildConfig to browserBuildConfig
    
    * rollup: use package.json#exports for esm entrypoints
    
    * chore: package.json
    
    * rollup config
    
    * sort
    
    * lint
    
    * prettier
    
    ---------
    
    Co-authored-by: Sojin Park <raon0211@toss.im>

--- tests/check-dist.spec.ts
+++ tests/check-dist.spec.ts
@@ -32,59 +32,69 @@ const ENTRYPOINTS = [
   './promise',
   './string',
   './compat',
-]
+];
 
 describe(`es-toolkit's package tarball`, () => {
-  it('configures all entrypoints correctly', async () => {
-    const packageJson = await getPackageJsonOfTarball();
-    const entrypoints = Object.keys(packageJson.exports);
+  it(
+    'configures all entrypoints correctly',
+    async () => {
+      const packageJson = await getPackageJsonOfTarball();
+      const entrypoints = Object.keys(packageJson.exports);
 
-    expect(entrypoints).toEqual([
-      ...ENTRYPOINTS,
-      './package.json'
-    ]);
-  }, { timeout: 30_000 });
+      expect(entrypoints).toEqual([...ENTRYPOINTS, './package.json']);
+    },
+    { timeout: 30_000 }
+  );
 
-  it('exports identical functions in CJS and ESM', async () => {
-    const tarball = await createPackageTarball();
-    const tmpdir = await createTmpDir();
+  it(
+    'exports identical functions in CJS and ESM',
+    async () => {
+      const tarball = await createPackageTarball();
+      const tmpdir = await createTmpDir();
 
-    const packageJson = {
-      "dependencies": {
-        "es-toolkit": tarball.path,
+      const packageJson = {
+        dependencies: {
+          'es-toolkit': tarball.path,
+        },
+      };
+
+      await fs.promises.writeFile(path.join(tmpdir, 'package.json'), JSON.stringify(packageJson, null, 2));
+      await execa('npm', ['install'], { cwd: tmpdir });
+
+      for (const entrypoint of ENTRYPOINTS) {
+        const cjsScript = `
+const toolkit = require("${path.join('es-toolkit', entrypoint)}");
+
+const exported = Object.entries(toolkit)
+  .map(([k, v]) => [k, typeof v])
+  .sort((x, y) => x[0].localeCompare(y[0]));
+
+console.log("${path.join('es-toolkit', entrypoint)}");
+console.log(exported);
+      `.trim();
+        const cjsScriptPath = path.join(tmpdir, 'script.cjs');
+
+        const esmScript = `
+const toolkit = await import("${path.join('es-toolkit', entrypoint)}");
+
+const exported = Object.entries(toolkit)
+  .map(([k, v]) => [k, typeof v])
+  .sort((x, y) => x[0].localeCompare(y[0]));
+
+console.log("${path.join('es-toolkit', entrypoint)}");
+console.log(exported);
+      `.trim();
+        const esmScriptPath = path.join(tmpdir, 'script.mjs');
+
+        await fs.promises.writeFile(cjsScriptPath, cjsScript);
+        await fs.promises.writeFile(esmScriptPath, esmScript);
+
+        const cjsResult = await execa('node', [cjsScriptPath]);
+        const esmResult = await execa('node', [esmScriptPath]);
+
+        expect(cjsResult.stdout).toEqual(esmResult.stdout);
       }
-    }
-
-    await fs.promises.writeFile(path.join(tmpdir, 'package.json'), JSON.stringify(packageJson, null, 2));
-    await execa('npm', ['install'], { cwd: tmpdir });
-
-    for (const entrypoint of ENTRYPOINTS) {
-      const cjsScript = `
-const toolkit = require("${path.join("es-toolkit", entrypoint)}");
-
-const exported = Object.entries(toolkit).map(([k, v]) => [k, typeof v]);
-
-console.log(exported);
-      `.trim();
-      const cjsScriptPath = path.join(tmpdir, 'script.cjs');
-
-      const esmScript = `
-const toolkit = await import("${path.join("es-toolkit", entrypoint)}");
-
-const exported = Object.entries(toolkit).map(([k, v]) => [k, typeof v]);
-
-console.log(exported);
-      `.trim();
-      const esmScriptPath = path.join(tmpdir, 'script.mjs');
-
-      await fs.promises.writeFile(cjsScriptPath, cjsScript);
-      await fs.promises.writeFile(esmScriptPath, esmScript);
-
-      const cjsResult = await execa('node', [cjsScriptPath])
-      const esmResult = await execa('node', [esmScriptPath])
-
-      expect(cjsResult.stdout).toEqual(esmResult.stdout);
-    }
-
-  }, { timeout: 60_000 });
-})
+    },
+    { timeout: 60_000 }
+  );
+});
