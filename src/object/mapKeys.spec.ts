904d596b78405fa77bcc849dc14732c6f6d070ee
raon0211 <raon0211@toss.im>
2024-07-25 00:58:28 +0900

    Fix interface

--- src/object/mapKeys.spec.ts
+++ src/object/mapKeys.spec.ts
@@ -3,20 +3,20 @@ import { mapKeys } from './mapKeys';
 
 describe('mapKeys', () => {
   it('should iterate over and map the object using its own string keys', () => {
-    expect(mapKeys({ a: 1, b: 2, c: 3 }, ({ key }) => `${key}a`)).toEqual({ aa: 1, ba: 2, ca: 3 });
+    expect(mapKeys({ a: 1, b: 2, c: 3 }, (_, key) => `${key}a`)).toEqual({ aa: 1, ba: 2, ca: 3 });
   });
 
   it('should iterate over and map the object using its own number keys', () => {
-    expect(mapKeys({ 1: 'a', 2: 'b', 3: 'c' }, ({ key }) => key * 2)).toEqual({ 2: 'a', 4: 'b', 6: 'c' });
+    expect(mapKeys({ 1: 'a', 2: 'b', 3: 'c' }, (_, key) => key * 2)).toEqual({ 2: 'a', 4: 'b', 6: 'c' });
   });
 
   it('should pass the value corresponding to the current key into the iteratee', () => {
-    expect(mapKeys({ a: 1, b: 2, c: 3 }, ({ value }) => value)).toEqual({ 1: 1, 2: 2, 3: 3 });
+    expect(mapKeys({ a: 1, b: 2, c: 3 }, (value) => value)).toEqual({ 1: 1, 2: 2, 3: 3 });
   });
 
   it('should pass the cloned object into the iteratee', () => {
     expect(
-      mapKeys({ a: 1, b: 2, c: 3 }, ({ key, object }) => {
+      mapKeys({ a: 1, b: 2, c: 3 }, (_, key, object) => {
         object[`${key}${key}`] = 4;
         return Object.keys(object).join('');
       })
--- src/object/mapValues.spec.ts
+++ src/object/mapValues.spec.ts
@@ -3,16 +3,16 @@ import { mapValues } from './mapValues';
 
 describe('mapValues', () => {
   it('should iterate over and map the object using its own values', () => {
-    expect(mapValues({ a: 1, b: 2, c: 3 }, ({ value }) => ++value)).toEqual({ a: 2, b: 3, c: 4 });
+    expect(mapValues({ a: 1, b: 2, c: 3 }, (value) => ++value)).toEqual({ a: 2, b: 3, c: 4 });
   });
 
   it('should pass the key corresponding to the current value into the iteratee', () => {
-    expect(mapValues({ a: 1, b: 2, c: 3 }, ({ key }) => key)).toEqual({ a: 'a', b: 'b', c: 'c' });
+    expect(mapValues({ a: 1, b: 2, c: 3 }, (_, key) => key)).toEqual({ a: 'a', b: 'b', c: 'c' });
   });
 
   it('should pass the cloned object into the iteratee', () => {
     expect(
-      mapValues({ a: 1, b: 2, c: 3 }, ({ key, value, object }) => {
+      mapValues({ a: 1, b: 2, c: 3 }, (value, key, object) => {
         object[key] = value * 11;
         return Object.values(object).join('');
       })
