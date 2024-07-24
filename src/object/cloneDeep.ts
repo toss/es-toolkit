948439e903621a562351d120abdd67ee2907851f
hainan <hainan_612@yahoo.com>
2024-07-24 17:39:52 +0800

    lint

--- src/object/cloneDeep.ts
+++ src/object/cloneDeep.ts
@@ -85,8 +85,10 @@ export function cloneDeep<T>(obj: T): Resolved<T> {
     return result as Resolved<T>;
   }
 
-  if (obj instanceof ArrayBuffer ||
-    (typeof window.SharedArrayBuffer !== 'undefined' && obj instanceof SharedArrayBuffer)) {
+  if (
+    obj instanceof ArrayBuffer ||
+    (typeof window.SharedArrayBuffer !== 'undefined' && obj instanceof SharedArrayBuffer)
+  ) {
     return obj.slice(0) as Resolved<T>;
   }
 
