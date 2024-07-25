2121e784365008e1c632e5b03482ac63b89e83cc
raon0211 <raon0211@toss.im>
2024-07-25 10:04:31 +0900

    test: Check test

--- src/object/mapKeys.spec.ts
+++ src/object/mapKeys.spec.ts
@@ -1,23 +1,23 @@
 import { describe, expect, it } from 'vitest';
-import { mapKeys } from './mapKeys';
+import { mapKey } from './mapKey';
 
-describe('mapKeys', () => {
+describe('mapKey', () => {
   it('should iterate over and map the object using its own string keys', () => {
-    expect(mapKeys({ a: 1, b: 2, c: 3 }, (_, key) => `${key}a`)).toEqual({ aa: 1, ba: 2, ca: 3 });
+    expect(mapKey({ a: 1, b: 2, c: 3 }, (_, key) => `${key}a`)).toEqual({ aa: 1, ba: 2, ca: 3 });
   });
 
   it('should iterate over and map the object using its own number keys', () => {
-    expect(mapKeys({ 1: 'a', 2: 'b', 3: 'c' }, (_, key) => key * 2)).toEqual({ 2: 'a', 4: 'b', 6: 'c' });
+    expect(mapKey({ 1: 'a', 2: 'b', 3: 'c' }, (_, key) => key * 2)).toEqual({ 2: 'a', 4: 'b', 6: 'c' });
   });
 
   it('should pass the value corresponding to the current key into the iteratee', () => {
-    expect(mapKeys({ a: 1, b: 2, c: 3 }, (value) => value)).toEqual({ 1: 1, 2: 2, 3: 3 });
+    expect(mapKey({ a: 1, b: 2, c: 3 }, (value) => value)).toEqual({ 1: 1, 2: 2, 3: 3 });
   });
 
   it('should pass the cloned object into the iteratee', () => {
     const object: Record<string, number> = { a: 1, b: 2, c: 3 };
     expect(
-      mapKeys(object, (_, key, object) => {
+      mapKey(object, (_, key, object) => {
         object[`${key}${key}`] = 4;
         return `${key}${key}`;
       })
