70ae7c59fccaa752589049062ca7edc17bfc1b94
StyleShit <evyatar.daud@gmail.com>
2024-07-26 16:07:19 +0300

    feat(without): improve types

--- src/array/without.spec.ts
+++ src/array/without.spec.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, expectTypeOf, it } from 'vitest';
 import { without } from './without';
 
 describe('without', () => {
   it('should return an empty array when the input array is empty', () => {
-    expect(without([], 1, 2, 3)).toEqual([]);
+    expect(without([] as number[], 1, 2, 3)).toEqual([]);
   });
 
   it('should return the same array when no values are provided to exclude', () => {
@@ -16,12 +16,12 @@ describe('without', () => {
   });
 
   it('should handle cases where none of the specified values are in the array', () => {
-    expect(without([1, 2, 3], 4, 5)).toEqual([1, 2, 3]);
+    expect(without([1, 2, 3], 4 as any, 5)).toEqual([1, 2, 3]);
   });
 
   it('should handle cases with different types of values', () => {
-    expect(without([1, '2', 3, '4'], 2, '4')).toEqual([1, '2', 3]);
-    expect(without([1, '1', 2, '2'], 1, '2')).toEqual(['1', 2]);
+    expect(without([1, '2', 3, '4'], 2 as any, '4')).toEqual([1, '2', 3]);
+    expect(without([1, '1', 2, '2'], 1 as any, '2')).toEqual(['1', 2]);
   });
 
   it('should handle NaN values correctly', () => {
@@ -31,5 +31,27 @@ describe('without', () => {
   it('should treat +0 and -0 as equal', () => {
     expect(without([0, -0, 1, 2], 0)).toEqual([1, 2]);
     expect(without([0, -0, 1, 2], -0)).toEqual([1, 2]);
+  });
+
+  it('should handle parameter types properly', () => {
+    // @ts-expect-error - The second parameter is not in the array
+    without([1, 2, 3], 4);
+
+    // @ts-expect-error - The second parameter is not in the array
+    without([1, 2, 3], '2');
+
+    // @ts-expect-error - The third parameter is not in the array
+    without([1, 2, 3], 2, '4');
+  });
+
+  it('should handle return types properly', () => {
+    expectTypeOf(without([1, 2, 3], 2)).toEqualTypeOf<[1, 3]>();
+    expectTypeOf(without([1, 2, 3], 1, 2, 3)).toEqualTypeOf<[]>();
+    expectTypeOf(without([1, 2, 3, 2, 2, 2], 2)).toEqualTypeOf<[1, 3]>();
+    expectTypeOf(without(['a', 'b', 'c'], 'a')).toEqualTypeOf<['b', 'c']>();
+
+    expectTypeOf(without([], 2 as never)).toEqualTypeOf<[]>();
+    expectTypeOf(without([1, '1', 2] as number[], 1)).toEqualTypeOf<number[]>();
+    expectTypeOf(without([1, '2', 3], 2 as any)).toEqualTypeOf<[1, '2', 3]>();
   });
 });
--- src/array/without.ts
+++ src/array/without.ts
@@ -4,9 +4,10 @@
  * It correctly excludes `NaN`, as it compares values using [SameValueZero](https://tc39.es/ecma262/multipage/abstract-operations.html#sec-samevaluezero).
  *
  * @template T The type of elements in the array.
+ * @template V The type of values to exclude.
  * @param {T[]} array - The array to filter.
- * @param {...T[]} values - The values to exclude.
- * @returns {T[]} A new array without the specified values.
+ * @param {...V[]} values - The values to exclude.
+ * @returns {Without<T, V>} A new array without the specified values.
  *
  * @example
  * // Removes the specified values from the array
@@ -18,7 +19,26 @@
  * without(['a', 'b', 'c', 'a'], 'a');
  * // Returns: ['b', 'c']
  */
-export function without<T>(array: readonly T[], ...values: T[]): T[] {
+export function without<const T extends any[], const V extends T[number]>(array: T, ...values: V[]): Without<T, V> {
   const valuesSet = new Set(values);
-  return array.filter(item => !valuesSet.has(item));
+  return array.filter(item => !valuesSet.has(item as never)) as never;
 }
+
+type Without<Items extends any[], Exclude extends Items[number], Out extends any[] = []> = Items extends []
+  ? Out
+  : IsAny<Exclude> extends true
+    ? Items
+    : IsNever<Exclude> extends true
+      ? Items
+      : Items extends [infer First, ...infer Rest]
+        ? First extends Exclude
+          ? Without<Rest, Exclude, Out>
+          : Without<Rest, Exclude, [...Out, First]>
+        : Array<Items[number]>;
+
+/**
+ * @see https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
+ */
+type IsAny<T> = 0 extends 1 & T ? true : false;
+
+type IsNever<T> = [T] extends [never] ? true : false;
