0c82d6f9914b1f655cf31048e567f9285f55fd43
dayongkr <dayongkr@gmail.com>
2024-07-30 17:57:22 +0900

    fix jsdoc

--- src/compat/array/flatten.ts
+++ src/compat/array/flatten.ts
@@ -3,9 +3,9 @@
  *
  * @template T - The type of elements within the array.
  * @template D - The depth to which the array should be flattened.
- * @param {T[] | object} value - The array to flatten.
+ * @param {T[] | object} value - The object to flatten.
  * @param {D} depth - The depth level specifying how deep a nested array structure should be flattened. Defaults to 1.
- * @returns {Array<FlatArray<T[], D>>} A new array that has been flattened.
+ * @returns {Array<FlatArray<T[], D>> | []} A new array that has been flattened.
  *
  * @example
  * const arr = flatten([1, [2, 3], [4, [5, 6]]], 1);
@@ -14,7 +14,7 @@
  * const arr = flatten([1, [2, 3], [4, [5, 6]]], 2);
  * // Returns: [1, 2, 3, 4, 5, 6]
  */
-export function flatten<T, D extends number = 1>(value: T[] | object, depth = 1 as D): Array<FlatArray<T[], D>> {
+export function flatten<T, D extends number = 1>(value: T[] | object, depth = 1 as D): Array<FlatArray<T[], D>> | [] {
   const result: Array<FlatArray<T[], D>> = [];
   const flooredDepth = Math.floor(depth);
 
