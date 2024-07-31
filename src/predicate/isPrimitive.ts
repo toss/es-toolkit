2c0168756d11471d0f2d13fff5b98e15c67d56dd
ssi02014 <ssi02014@naver.com>
2024-07-31 21:43:43 +0900

    docs: fix isPrimitive jsdoc

--- src/predicate/isPrimitive.ts
+++ src/predicate/isPrimitive.ts
@@ -3,14 +3,14 @@
  * JavaScript primitives include null, undefined, strings, numbers, booleans, symbols, and bigints.
  *
  * @param {unknown} value The value to check.
- * @returns {x is
+ * @returns {value is
  *   | null
  *   | undefined
  *   | string
  *   | number
  *   | boolean
  *   | symbol
- *   | bigint} Returns true if `x` is a primitive, false otherwise.
+ *   | bigint} Returns true if `value` is a primitive, false otherwise.
  */
 export function isPrimitive(value: unknown): value is null | undefined | string | number | boolean | symbol | bigint {
   return value == null || (typeof value !== 'object' && typeof value !== 'function');
