46b131ecbb0629d7e35c8c6ab316b9991ce7bf32
dayongkr <dayongkr@gmail.com>
2024-08-01 17:00:38 +0900

    feat(flattenDeep,flattenDepth): add compatibility with lodash

---
+++ src/compat/array/flattenDepth.ts
@@ -0,0 +1,24 @@
+import { flatten } from './flatten';
+
+/**
+ * Flattens an array up to the specified depth.
+ *
+ * @template T - The type of elements within the array.
+ * @template D - The depth to which the array should be flattened.
+ * @param {T[] | object} value - The value to flatten.
+ * @param {D} depth - The depth level specifying how deep a nested array structure should be flattened. Defaults to 1.
+ * @returns {Array<FlatArray<T[], D>> | []} A new array that has been flattened.
+ *
+ * @example
+ * const arr = flatten([1, [2, 3], [4, [5, 6]]], 1);
+ * // Returns: [1, 2, 3, 4, [5, 6]]
+ *
+ * const arr = flatten([1, [2, 3], [4, [5, 6]]], 2);
+ * // Returns: [1, 2, 3, 4, 5, 6]
+ */
+export function flattenDepth<T, D extends number = 1>(
+  value: T[] | object,
+  depth = 1 as D
+): Array<FlatArray<T[], D>> | [] {
+  return flatten(value, depth);
+}
