e5c90334df7a9cfe798993223bf83466c69280ce
D-Sketon <2055272094@qq.com>
2024-08-03 12:14:43 +0800

    fix: redundant prototype copy

--- src/function/bind.ts
+++ src/function/bind.ts
@@ -1,3 +1,29 @@
+/**
+ *
+ * Creates a function that invokes `func` with the `this` binding of `thisArg` and `partials` prepended to the arguments it receives.
+ *
+ * The `bind.placeholder` value, which defaults to a `symbol`, may be used as a placeholder for partially applied arguments.
+ *
+ * **Note:** Unlike native `Function#bind`, this method doesn't set the `length` property of bound functions.
+ *
+ * @param {(...args: any[]) => any} func  The function to bind.
+ * @param {any} thisArg  The `this` binding of `func`.
+ * @param {any[]} partials  The arguments to be partially applied.
+ * @returns {(...args: any[]) => any} Returns the new bound function.
+ *
+ * @example
+ * function greet(greeting, punctuation) {
+ *   return greeting + ' ' + this.user + punctuation;
+ * }
+ * const object = { user: 'fred' };
+ * let bound = bind(greet, object, 'hi');
+ * bound('!');
+ * // => 'hi fred!'
+ *
+ * bound = bind(greet, object, bind.placeholder, '!');
+ * bound('hi');
+ * // => 'hi fred!'
+ */
 export function bind(func: (...args: any[]) => any, thisArg?: any, ...partials: any[]): (...args: any[]) => any {
   const wrapper = function (this: any, ...args: any[]) {
     let index = 0;
@@ -16,7 +42,6 @@ export function bind(func: (...args: any[]) => any, thisArg?: any, ...partials: 
     }
     return func.apply(thisArg, result);
   };
-  wrapper.prototype = func.prototype;
   return wrapper;
 }
 
