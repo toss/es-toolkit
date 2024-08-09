9109e890788fcfd5a7346a5b28c597389b56109c
raon0211 <raon0211@toss.im>
2024-08-09 09:52:02 +0900

    revert: Revert Sandpack components, since it currently breaks the build
    Revert "docs: Implement Sandpack component for interactive examples in documentation (#338)"
    
    This reverts commit db79ff335534f5b263ebde11e5c633aee7ad58b0.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,4 @@
 import DefaultTheme from 'vitepress/theme';
-import Sandpack from '../../components/Sandpack.vue';
 import './index.css';
 
-export default {
-  ...DefaultTheme,
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp(ctx);
-    ctx.app.component('Sandpack', Sandpack);
-  },
-};
+export default DefaultTheme;
