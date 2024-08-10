bf33cfbca5547d8791af29be529be89ecc209e7c
Dayong Lee <dayongkr@gmail.com>
2024-08-10 16:30:32 +0900

    feat(orderBy, isSymbol): add compatibility with lodash  (#357)
    * Add internal functions
    
    * add orderBy with compat
    
    * Add docs
    
    * Fix typo
    
    * Split the code for readability
    
    * Change convert to path logic and variable name for readability
    
    * Add test cases and js doc in isKey and isSymbol
    
    * Add testcase and change logic of isKey
    
    * change bench category name
    
    * split converToPropertyName for testing
    
    * Add case for coverage
    
    * fix type
    
    * Change using getTage to instanceof
    
    * move to predicate
    
    * Add doc
    
    * Add bench and description in isSymbol
    
    * Fix type and test case
    
    * Simplify function names
    
    * Feat: handle the deep path like keys

---
+++ src/compat/_internal/isKey.ts
@@ -0,0 +1,34 @@
+import { isSymbol } from '../predicate/isSymbol';
+
+/**  Matches any deep property path. (e.g. `a.b[0].c`)*/
+const regexIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\\]|\\.)*?\1)\]/;
+/**  Matches any word character (alphanumeric & underscore).*/
+const regexIsPlainProp = /^\w*$/;
+
+/**
+ * Checks if `value` is a property name and not a property path. (It's ok that the `value` is not in the keys of the `object`)
+ * @param {unknown} value The value to check.
+ * @param {unknown} object The object to query.
+ * @returns {boolean} Returns `true` if `value` is a property name, else `false`.
+ *
+ * @example
+ * isKey('a', { a: 1 });
+ * // => true
+ *
+ * isKey('a.b', { a: { b: 2 } });
+ * // => false
+ */
+export function isKey(value?: unknown, object?: unknown): boolean {
+  if (Array.isArray(value)) {
+    return false;
+  }
+
+  if (typeof value === 'number' || typeof value === 'boolean' || value == null || isSymbol(value)) {
+    return true;
+  }
+
+  return (
+    (typeof value === 'string' && (regexIsPlainProp.test(value) || !regexIsDeepProp.test(value))) ||
+    (object != null && Object.hasOwn(object, value as PropertyKey))
+  );
+}
---
+++ src/compat/predicate/isSymbol.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from 'vitest';
+import { isSymbol } from './isSymbol';
+import { falsey } from '../_internal/falsey';
+import { stubFalse } from '../_internal/stubFalse';
+import { args } from '../_internal/args';
+import { slice } from '../_internal/slice';
+
+describe('isSymbol', () => {
+  it('should return `true` for symbols', () => {
+    expect(isSymbol(Symbol('a'))).toBe(true);
+    expect(isSymbol(Object(Symbol('a')))).toBe(true);
+  });
+
+  it('should return `false` for non-symbols', () => {
+    const expected = falsey.map(stubFalse);
+    const actual = falsey.map(isSymbol);
+
+    expect(actual).toEqual(expected);
+
+    expect(isSymbol(args)).toBe(false);
+    expect(isSymbol([1, 2, 3])).toBe(false);
+    expect(isSymbol(true)).toBe(false);
+    expect(isSymbol(new Date())).toBe(false);
+    expect(isSymbol(new Error())).toBe(false);
+    expect(isSymbol(slice)).toBe(false);
+    expect(isSymbol({ 0: 1, length: 1 })).toBe(false);
+    expect(isSymbol(1)).toBe(false);
+    expect(isSymbol(/x/)).toBe(false);
+    expect(isSymbol('a')).toBe(false);
+  });
+});
