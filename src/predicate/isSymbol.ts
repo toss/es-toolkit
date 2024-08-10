4d46377d9b7f6a513103592fc4d2d18dafc4830d
D-Sketon <2055272094@qq.com>
2024-08-10 09:16:18 +0800

    feat(isSymbol): implement isSymbol (#359)
    * feat: implement isSymbol
    
    * fix: make pure

---
+++ src/predicate/isSymbol.ts
@@ -0,0 +1,11 @@
+/**
+ * Check whether a value is a symbol.
+ *
+ * This function can also serve as a type predicate in TypeScript, narrowing the type of the argument to `symbol`.
+ *
+ * @param {unknown} value The value to check.
+ * @returns {value is symbol} Returns `true` if `value` is a symbol, else `false`.
+ */
+export function isSymbol(value: unknown): value is symbol {
+  return typeof value === 'symbol';
+}
