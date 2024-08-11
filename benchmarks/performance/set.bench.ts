cd26244cedde614ed23173e9719b828a7ad4b25a
dayongkr <dayongkr@gmail.com>
2024-08-11 14:42:34 +0900

    set: Move and import from compat

--- benchmarks/performance/set.bench.ts
+++ benchmarks/performance/set.bench.ts
@@ -1,13 +1,13 @@
 import { describe, bench } from 'vitest';
-import { set } from 'es-toolkit';
+import { set as setToolkitCompat } from 'es-toolkit/compat';
 import { set as lodashSet } from 'lodash';
 
 describe('set', () => {
   bench('es-toolkit/set-1', () => {
-    set({}, 'a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z', 1);
+    setToolkitCompat({}, 'a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z', 1);
   });
   bench('es-toolkit/set-2', () => {
-    set({}, 'a[0][1][2][3][4][5][6]', 1);
+    setToolkitCompat({}, 'a[0][1][2][3][4][5][6]', 1);
   });
 
   bench('lodash/set-1', () => {
