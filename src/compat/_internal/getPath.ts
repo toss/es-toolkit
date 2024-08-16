60169aa58a4358bc1eba2d5b07f5ec6103722542
dayongkr <dayongkr@gmail.com>
2024-08-16 23:03:26 +0900

    Make clean getPath

--- src/compat/_internal/getPath.ts
+++ src/compat/_internal/getPath.ts
@@ -4,9 +4,10 @@ import { toPath } from './toPath';
 /**
  * Get the `path` (property name) from the `key` (property name or property path).
  *
- * @param {string | string[]} key - The `key` (property name or property path) to convert.
- * @param {object} object - The object to query.
- * @returns {string | string[]} The converted key (only property name).
+ * @template T - The type of the object.
+ * @param {((item: T) => unknown) | string | string[]} key - The `key` (property name or property path or custom key function) to query.
+ * @param {T} object - The object to query.
+ * @returns {((item: T) => unknown) | string | string[]} The converted key (only property name or custom key function).
  */
 export function getPath<T extends object>(
   key: ((item: T) => unknown) | string | string[],
@@ -14,19 +15,19 @@ export function getPath<T extends object>(
 ): ((item: T) => unknown) | string | string[] {
   if (Array.isArray(key)) {
     const path = [];
-    let keyOwner: object = object;
+    let current: object = object;
 
     for (let i = 0; i < key.length; i++) {
       const k = key[i];
 
-      if (isKey(k, keyOwner)) {
-        keyOwner = keyOwner[k as keyof typeof keyOwner];
+      if (isKey(k, current)) {
+        current = current[k as keyof typeof current];
         path.push(k);
       } else {
         const keys = toPath(k);
 
         for (let i = 0; i < keys.length; i++) {
-          keyOwner = keyOwner[keys[i] as keyof typeof keyOwner];
+          current = current[keys[i] as keyof typeof current];
           path.push(keys[i]);
         }
       }
@@ -34,9 +35,10 @@ export function getPath<T extends object>(
 
     return path;
   }
-  if (typeof key === 'function') {
+
+  if (typeof key === 'function' || isKey(key, object)) {
     return key;
   }
 
-  return isKey(key, object) ? key : toPath(key);
+  return toPath(key);
 }
