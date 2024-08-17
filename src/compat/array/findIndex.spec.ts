f08fbd1b6a8ee65f058ddffadd44fd2e8c156407
Seongjee Kim <lsvk3577@naver.com>
2024-08-17 10:31:23 +0900

    chore: remove unused import (#388)

--- src/compat/array/findIndex.spec.ts
+++ src/compat/array/findIndex.spec.ts
@@ -1,5 +1,4 @@
 import { describe, expect, it } from 'vitest';
-import { empties } from '../_internal/empties';
 import { findIndex } from './findIndex';
 import { slice } from '../_internal/slice';
 
