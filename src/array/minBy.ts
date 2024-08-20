d75b549aa7d5b2e63ae2ccfeafef4180a0ab05dd
Yota Toyama <raviqqe@gmail.com>
2024-08-20 11:40:21 +0900

    Fix typing of `minBy`

--- src/array/minBy.ts
+++ src/array/minBy.ts
@@ -10,7 +10,7 @@
  * minBy([{ a: 1 }, { a: 2 }, { a: 3 }], x => x.a); // Returns: { a: 1 }
  * minBy([], x => x.a); // Returns: undefined
  */
-export function minBy<T>(items: T[], getValue: (element: T) => number): T {
+export function minBy<T>(items: T[], getValue: (element: T) => number): T | undefined {
   let minElement = items[0];
   let min = Infinity;
 
