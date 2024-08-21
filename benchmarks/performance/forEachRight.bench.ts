38e0061a6ab9240361172c832a8bf8a01be37db8
Dongho Kim <dongre2527@gmail.com>
2024-08-21 22:52:41 +0900

    style: Fix typo

--- benchmarks/performance/forEachRight.bench.ts
+++ benchmarks/performance/forEachRight.bench.ts
@@ -1,10 +1,10 @@
 import { bench, describe } from 'vitest';
-import { forEachRight as forEachRightTookit } from 'es-toolkit';
+import { forEachRight as forEachRightToolkit } from 'es-toolkit';
 import { forEachRight as forEachRightLodash } from 'lodash';
 
 describe('forEachRight', () => {
   bench('es-toolkit/forEachRight', () => {
-    forEachRightTookit([1, 2, 3, 4, 5, 6], x => x + 3);
+    forEachRightToolkit([1, 2, 3, 4, 5, 6], x => x + 3);
   });
 
   bench('lodash/forEachRight', () => {
