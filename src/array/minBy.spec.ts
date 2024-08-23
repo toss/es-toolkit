5daa7ee8bf17667e79b36ce5d5856577033c72dd
Yota Toyama <raviqqe@gmail.com>
2024-08-23 19:02:17 +0900

    Remove `const` annotations

--- src/array/minBy.spec.ts
+++ src/array/minBy.spec.ts
@@ -9,7 +9,7 @@ describe('minBy', () => {
       { name: 'Mark', age: 30 },
       { name: 'Nunu', age: 20 },
       { name: 'Overmars', age: 35 },
-    ] as const;
+    ];
     const result = minBy<Person>(people, person => person.age);
     expect(result).toEqual({ name: 'Nunu', age: 20 });
   });
@@ -19,13 +19,13 @@ describe('minBy', () => {
       { name: 'Mark', age: 30 },
       { name: 'Nunu', age: 20 },
       { name: 'Overmars', age: 20 },
-    ] as const;
+    ];
     const result = minBy<Person>(people, person => person.age);
     expect(result).toEqual({ name: 'Nunu', age: 20 });
   });
 
   it('if array is single-element, return unique element of array', () => {
-    const people = [{ name: 'Mark', age: 25 }] as const;
+    const people = [{ name: 'Mark', age: 25 }];
     const result = minBy(people, person => person.age);
     expect(result).toEqual({ name: 'Mark', age: 25 });
   });
