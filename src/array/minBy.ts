b2b99f45c569351cd52901c02f3c5b84e218d8e4
Yota Toyama <raviqqe@gmail.com>
2024-08-23 18:52:25 +0900

    Fix return type

--- src/array/minBy.ts
+++ src/array/minBy.ts
@@ -5,7 +5,7 @@
  * @template T - The type of elements in the array.
  * @param {T[]} items The array of elements to search.
  * @param {(element: T) => number} getValue A function that selects a numeric value from each element.
- * @returns {T} The element with the minimum value as determined by the `getValue` function.
+ * @returns {T | undefined} The element with the minimum value as determined by the `getValue` function.
  * @example
  * minBy([{ a: 1 }, { a: 2 }, { a: 3 }], x => x.a); // Returns: { a: 1 }
  * minBy([], x => x.a); // Returns: undefined
