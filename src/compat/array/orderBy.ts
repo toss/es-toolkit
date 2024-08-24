b73fbb84af438a5f510dd1de20c3cd4d5ac47eda
dayongkr <dayongkr@gmail.com>
2024-08-25 02:26:49 +0900

    Change variable name

--- src/compat/array/orderBy.ts
+++ src/compat/array/orderBy.ts
@@ -8,14 +8,14 @@ const memoizedToPath = memoizeCapped(toPath);
 /**
  * Sorts an array of objects based on multiple properties and their corresponding order directions.
  *
- * This function takes an array of objects, an array of keys to sort by, and an array of order directions.
+ * This function takes an array of objects, an array of criteria to sort by, and an array of order directions.
  * It returns the sorted array, ordering by each key according to its corresponding direction
  * ('asc' for ascending or 'desc' for descending). If values for a key are equal,string
  * it moves to the next key to determine the order.
  *
  * @template T - The type of elements in the array.
  * @param { T[] | object | null | undefined} collection - The array of objects to be sorted.
- * @param {((item: T) => unknown) | PropertyKey | Array<((item: T) => unknown) | PropertyKey | PropertyKey[]>} keys - An array of keys (property names or property paths or custom key functions) to sort by.
+ * @param {((item: T) => unknown) | PropertyKey | Array<((item: T) => unknown) | PropertyKey | PropertyKey[]>} criteria - An array of criteria (property names or property paths or custom key functions) to sort by.
  * @param {unknown | unknown[]} orders - An array of order directions ('asc' for ascending or 'desc' for descending).
  * @returns {T[]} - The sorted array.
  *
@@ -38,7 +38,7 @@ const memoizedToPath = memoizeCapped(toPath);
  */
 export function orderBy<T>(
   collection: T[] | object | null | undefined,
-  keys?: ((item: T) => unknown) | PropertyKey | Array<((item: T) => unknown) | PropertyKey | PropertyKey[]>,
+  criteria?: ((item: T) => unknown) | PropertyKey | Array<((item: T) => unknown) | PropertyKey | PropertyKey[]>,
   orders?: unknown | unknown[]
 ): T[] {
   if (collection == null) {
@@ -49,59 +49,59 @@ export function orderBy<T>(
     collection = Object.values(collection);
   }
 
-  if (!Array.isArray(keys)) {
-    keys = keys == null ? [] : [keys];
+  if (!Array.isArray(criteria)) {
+    criteria = criteria == null ? [] : [criteria];
   }
 
   if (!Array.isArray(orders)) {
     orders = orders == null ? [] : [orders];
   }
 
-  const getValueByPath = (path: PropertyKey | ((item: T) => unknown) | PropertyKey[], object: T) => {
+  const getValueByCriterion = (criterion: PropertyKey | ((item: T) => unknown) | PropertyKey[], object: T) => {
     if (object == null) {
       return object;
     }
 
-    if (typeof path === 'function') {
-      return path(object);
+    if (typeof criterion === 'function') {
+      return criterion(object);
     }
 
-    // When the path is an array with a single element, we have to check whether it is a key or a deep property.
-    if (Array.isArray(path) && path.length === 1) {
-      path = path[0];
+    // When the criterion is an array with a single element, we have to check whether it is a key or a deep property.
+    if (Array.isArray(criterion) && criterion.length === 1) {
+      criterion = criterion[0];
     }
 
-    // When the path is a key, use the key directly
-    if (isKey(path, object)) {
-      return object[path as keyof T];
+    // When the criterion is a key, use the key directly
+    if (isKey(criterion, object)) {
+      return object[criterion as keyof T];
     }
 
-    // Convert the deep property to a path array
-    if (!Array.isArray(path)) {
-      path = memoizedToPath(path as string);
+    // Convert the deep property to a criterion array
+    if (!Array.isArray(criterion)) {
+      criterion = memoizedToPath(criterion as string);
     }
 
     let target: object = object;
 
-    for (let i = 0; i < path.length && target != null; i++) {
-      target = target[path[i] as keyof typeof target];
+    for (let i = 0; i < criterion.length && target != null; i++) {
+      target = target[criterion[i] as keyof typeof target];
     }
 
     return target;
   };
 
-  const withCriteria = (collection as T[]).slice().map(item => ({
-    // Prepare the criteria before sorting is more efficient.
-    criteria: keys.map(key => getValueByPath(key, item)),
+  const collectionWtihValues = (collection as T[]).slice().map(item => ({
+    // Prepare the values that will be used for sorting, this way is more efficient than getting the values in the comparator function.
+    values: criteria.map(criterion => getValueByCriterion(criterion, item)),
     original: item,
   }));
 
-  return withCriteria
+  return collectionWtihValues
     .sort((a, b) => {
-      for (let i = 0; i < keys.length; i++) {
+      for (let i = 0; i < criteria.length; i++) {
         const order = String((orders as unknown[])[i]); // For Object('desc') case
 
-        const comparedResult = compareValues(a.criteria[i], b.criteria[i], order);
+        const comparedResult = compareValues(a.values[i], b.values[i], order);
 
         if (comparedResult !== 0) {
           return comparedResult;
