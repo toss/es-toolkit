02643bcbe372a3c373b0d93ad356467d106e6819
mattiacoll <64754556+mattiacoll@users.noreply.github.com>
2024-08-26 01:12:57 +1200

    feat(lowerFirst): add `lowerFirst` function (#403)
    * feat(lowerFirst): Add lowerFirst function
    
    * Add lowerFirst benchmark
    
    * Add lowerFirst english docs
    
    ---------
    
    Co-authored-by: Sojin Park <raon0211@toss.im>

---
+++ src/string/lowerFirst.ts
@@ -0,0 +1,14 @@
+/**
+ * Converts the first character of string to lower case.
+ *
+ * @param {string} str - The string that is to be changed
+ * @returns {string} - The converted string.
+ *
+ * @example
+ * const convertedStr1 = lowerCase('fred') // returns 'fred'
+ * const convertedStr2 = lowerCase('Fred') // returns 'Fred'
+ * const convertedStr3 = lowerCase('FRED') // returns 'FRED'
+ */
+export const lowerFirst = (str: string): string => {
+  return str.substring(0, 1).toLowerCase() + str.substring(1);
+};
