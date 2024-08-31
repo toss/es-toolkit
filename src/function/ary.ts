023b0e77e65381da4ea02d1118cb2b5edfa393c1
Dongho Kim <dongre2527@gmail.com>
2024-08-31 14:10:05 +0900

    docs(ary): Update docs for `ary`

--- src/function/ary.ts
+++ src/function/ary.ts
@@ -5,6 +5,16 @@
  * @param {F} func - The function to cap arguments for.
  * @param {number} n - The arity cap.
  * @returns {(...args: any[]) => ReturnType<F>} Returns the new capped function.
+ *
+ * @example
+ * function fn(a: number, b: number, c: number) {
+ *   return Array.from(arguments);
+ * }
+ *
+ * expect(ary(fn, 0)(1, 2, 3)).toEqual([]);
+ * expect(ary(fn, 1)(1, 2, 3)).toEqual([1]);
+ * expect(ary(fn, 2)(1, 2, 3)).toEqual([1, 2]);
+ * expect(ary(fn, 3)(1, 2, 3)).toEqual([1, 2, 3]);
  */
 export function ary<F extends (...args: any[]) => any>(func: F, n: number): (...args: any[]) => ReturnType<F> {
   return function (this: any, ...args: Parameters<F>) {
