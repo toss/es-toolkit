2eba4ae01c3a984ffefdb31d194b204ac43be576
D-Sketon <2055272094@qq.com>
2024-09-09 21:34:02 +0800

    docs: improve doc

--- src/compat/predicate/isRegExp.ts
+++ src/compat/predicate/isRegExp.ts
@@ -5,6 +5,13 @@ import { getTag } from '../_internal/getTag';
  *
  * @param {unknown} value The value to check.
  * @returns {boolean} Returns `true` if `value` is a RegExp, `false` otherwise.
+ *
+ * @example
+ * const value1 = /abc/;
+ * const value2 = '/abc/';
+ *
+ * console.log(isRegExp(value1)); // true
+ * console.log(isRegExp(value2)); // false
  */
 export function isRegExp(value?: unknown): value is RegExp {
   return getTag(value) === '[object RegExp]';
