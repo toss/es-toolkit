cb16f4426e65bad807a01d44c334dccc5da56e92
dayongkr <dayongkr@gmail.com>
2024-09-13 11:31:51 +0900

    Revert

--- src/compat/_internal/normalizeForCase.ts
+++ src/compat/_internal/normalizeForCase.ts
@@ -1,6 +1,6 @@
 export function normalizeForCase(str: string | object): string {
   if (typeof str === 'object') {
-    str = String(str);
+    str = str.toString();
   }
   // Remove constraction apostrophes
   str = str.replace(/['\u2019]/g, '');
