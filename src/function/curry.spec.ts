20ea6268b73ece7026eace4f6e6358b80fac83c6
seungro <seungro@toss.im>
2024-09-18 02:22:54 +0900

    fix eslint

--- src/function/curry.spec.ts
+++ src/function/curry.spec.ts
@@ -55,6 +55,6 @@ describe('curry', () => {
     const optionalFn = (a: number, b: number, c?: number, d?: number) => [a, b, c, d];
 
     expectTypeOf(curry(optionalFn)(1)(2)).parameters.toEqualTypeOf<[arg?: number | void]>();
-    expectTypeOf(curry(optionalFn, 2)(1)(2)).toEqualTypeOf<(number | undefined)[]>();
+    expectTypeOf(curry(optionalFn, 2)(1)(2)).toEqualTypeOf<Array<number | undefined>>();
   });
 });
