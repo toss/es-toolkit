83e97d5bff1f8e788dbae56143f7cd4efce7f97f
dayongkr <dayongkr@gmail.com>
2024-09-18 13:17:05 +0900

    Enhance Error log

--- .scripts/tests/transform-lodash-test.ts
+++ .scripts/tests/transform-lodash-test.ts
@@ -7,7 +7,6 @@ import { transformAssert } from './_internal/transform/assert';
 export default function transform(file: FileInfo, { jscodeshift }: API) {
   try {
     const root = jscodeshift(formatBrokenSyntax(file.source));
-
     transformLodashStable(root, jscodeshift);
     transformImport(root, jscodeshift);
     transformAssert(root, jscodeshift);
@@ -15,6 +14,7 @@ export default function transform(file: FileInfo, { jscodeshift }: API) {
     return root.toSource();
   } catch (error) {
     if (error instanceof Error) {
+      console.error(`File Path: ${file.path}`);
       console.error(`Error Messaging: ${error.message}`);
       console.error('Please resolve the error before continuing.');
       console.error('If you need help, please open an issue on GitHub.');
