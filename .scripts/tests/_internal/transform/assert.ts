ee671eac0d27c11ca7730511f6da598acaa6d4d1
dayongkr <dayongkr@gmail.com>
2024-09-18 11:48:22 +0900

    Update assert

--- .scripts/tests/_internal/transform/assert.ts
+++ .scripts/tests/_internal/transform/assert.ts
@@ -16,14 +16,36 @@ export function transformAssert(root: Collection, jscodeshift: JSCodeshift): voi
     })
     .replaceWith(({ node }) => {
       if (node.callee.type === 'MemberExpression' && node.callee.property.type === 'Identifier') {
-        if (node.callee.property.name === 'deepStrictEqual') {
-          const expectArguments =
-            node.arguments.length === 3 ? [node.arguments[0], node.arguments[2]] : [node.arguments[0]];
+        const expectArguments =
+          node.arguments.length === 3 ? [node.arguments[0], node.arguments[2]] : [node.arguments[0]];
+        const expect = jscodeshift.callExpression(jscodeshift.identifier('expect'), expectArguments);
 
-          return jscodeshift.memberExpression(
-            jscodeshift.callExpression(jscodeshift.identifier('expect'), expectArguments),
-            jscodeshift.callExpression(jscodeshift.identifier('toEqual'), [node.arguments[1]])
-          );
+        switch (node.callee.property.name) {
+          case 'deepStrictEqual':
+            return jscodeshift.memberExpression(
+              expect,
+              jscodeshift.callExpression(jscodeshift.identifier('toEqual'), [node.arguments[1]])
+            );
+          case 'deepEqual':
+            return jscodeshift.memberExpression(
+              expect,
+              jscodeshift.callExpression(jscodeshift.identifier('toEqual'), [node.arguments[1]])
+            );
+          case 'strictEqual':
+            return jscodeshift.memberExpression(
+              expect,
+              jscodeshift.callExpression(jscodeshift.identifier('toBe'), [node.arguments[1]])
+            );
+          case 'notEqual':
+            return jscodeshift.memberExpression(
+              expect,
+              jscodeshift.callExpression(jscodeshift.identifier('not.toBe'), [node.arguments[1]])
+            );
+          case 'notStrictEqual':
+            return jscodeshift.memberExpression(
+              expect,
+              jscodeshift.callExpression(jscodeshift.identifier('not.toBe'), [node.arguments[1]])
+            );
         }
       }
       return node;
