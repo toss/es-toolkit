6708daa86974c37dfd713e405077f49b24b8a568
Alex Shan <shan-mx@hotmail.com>
2024-09-18 15:16:01 +0800

    feat(isNumber): add `isNumber` function (#543)
    * feat(isNumber): add isNumber function
    
    * style: format
    
    * fix: align nan behavior

--- src/predicate/index.ts
+++ src/predicate/index.ts
@@ -16,3 +16,4 @@ export { isSymbol } from './isSymbol.ts';
 export { isString } from './isString.ts';
 export { isWeakMap } from './isWeakMap.ts';
 export { isWeakSet } from './isWeakSet.ts';
+export { isNumber } from './isNumber.ts';
