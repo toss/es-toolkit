a7b55af6aa9b4546eeb6a3c4b8888eba2d995b2e
hyesungoh <haesungoh414@gmail.com>
2024-09-22 17:00:25 +0900

    feat: add isError to predicate
    Co-authored-by: leeminhee119 <mhl98@dgu.ac.kr>

---
+++ src/predicate/isError.ts
@@ -0,0 +1,16 @@
+/**
+ * Checks if `value` is an Error object.
+ *
+ * @param {unknown} value The value to check.
+ * @returns {value is Error} Returns `true` if `value` is an Error object, `false` otherwise.
+ *
+ * @example
+ * ```typescript
+ * console.log(isError(new Error())); // true
+ * console.log(isError('Error')); // false
+ * console.log(isError({ name: 'Error', message: '' })); // false
+ * ```
+ */
+export function isError(value: unknown): value is Error {
+  return value instanceof Error;
+}
