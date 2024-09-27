9d47a978398507023bf055913f87914ed68e693c
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-09-27 22:07:02 +0900

    test: filter

---
+++ benchmarks/performance/filter.bench.ts
@@ -0,0 +1,27 @@
+import { filter as filterToolkit } from 'es-toolkit/compat';
+import { filter as filterLodash } from 'lodash';
+import { bench, describe } from 'vitest';
+
+const arr = [
+  { a: 0, b: true },
+  { a: 1, b: true },
+  { a: 0, b: false },
+];
+
+describe('filter', () => {
+  bench('es-toolkit/filter', () => {
+    filterToolkit([1, 2, 3], number => number % 2 === 0);
+    filterToolkit(arr, { b: true });
+    filterToolkit(arr, ['a', 1]);
+    filterToolkit(arr, items => items.b);
+    filterToolkit({ a: 1, b: 2, c: 3 }, 'b');
+  });
+
+  bench('lodash/filter', () => {
+    filterLodash([1, 2, 3], number => number % 2 === 0);
+    filterLodash(arr, { b: true });
+    filterLodash(arr, ['a', 1]);
+    filterLodash(arr, items => items.b);
+    filterLodash({ a: 1, b: 2, c: 3 }, 'b');
+  });
+});
