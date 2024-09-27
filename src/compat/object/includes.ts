6d67bf056158681a172936635f722cbafa02ab0f
dayongkr <dayongkr@gmail.com>
2024-09-27 17:09:30 +0900

    makes safe

--- src/compat/object/includes.ts
+++ src/compat/object/includes.ts
@@ -1,13 +1,12 @@
 import { isString } from '../predicate/isString';
 import { toInteger } from '../util/toInteger';
-
 /**
  * Checks if value is in source.
  * If source is a string, it's checked for a substring of value, otherwise SameValueZero is used for equality comparisons.
  * If fromIndex is negative, it's used as the offset from the end of source.
  *
  * @param {unknown[] | object | string | null | undefined} source - The source to search in.
- * @param {unknown} [target] - The target value to check.
+ * @param {unknown} [target] - The value to search for.
  * @param {unknown} [fromIndex] - The index to search from.
  * @param {unknown} [guard] - Enables use as an iteratee for methods like `_.every`.
  * @returns {boolean} `true` if the object includes the target value, `false` otherwise.
@@ -18,7 +17,7 @@ import { toInteger } from '../util/toInteger';
  * includes({ a: 1, b: 'a', c: NaN }, NaN); // true
  * includes({ [Symbol('sym1')]: 1 }, 1); // false
  */
-export function includes(
+export function includes<T>(
   source: readonly unknown[] | object | string | null | undefined,
   target?: unknown,
   fromIndex?: unknown,
@@ -35,15 +34,11 @@ export function includes(
       safeIndex = Math.max(0, source.length + safeIndex);
     }
 
-    if (safeIndex > source.length) {
+    if (isString(source) && (safeIndex > source.length || target instanceof RegExp)) {
       return false;
     }
 
-    // Typescript sets type of first argument of `String.prototype.includes` as `string`, but it can handle all types of `target` except `RegExp`.
-    // So it is safe to insert `target` directly to `String.prototype.includes` except `RegExp`.
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    return source.includes(target, safeIndex);
+    return source.includes(target as any, safeIndex);
   }
 
   const keys = Object.keys(source);
