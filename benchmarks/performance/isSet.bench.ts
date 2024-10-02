676b5251fcdcee8bf8cdc5438459e1b6fed879ff
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-10-02 20:22:29 +0900

    Update benchmarks/performance/isSet.bench.ts
    Co-authored-by: D-Sketon <2055272094@qq.com>

--- benchmarks/performance/isSet.bench.ts
+++ benchmarks/performance/isSet.bench.ts
@@ -1,7 +1,11 @@
 import { bench, describe } from 'vitest';
-import { isSet as isSetToolkit } from 'es-toolkit';
-import { isSet as isSetToolkitCompat } from 'es-toolkit/compat';
-import { isSet as isSetLodash } from 'lodash';
+import { isSet as isSetToolkit_ } from 'es-toolkit';
+import { isSet as isSetToolkitCompat_ } from 'es-toolkit/compat';
+import { isSet as isSetLodash_ } from 'lodash';
+
+const isSetToolkit = isSetToolkit_;
+const isSetToolkitCompat = isSetToolkitCompat_;
+const isSetLodash = isSetLodash_;
 
 describe('isSet', () => {
   bench('es-toolkit/isSet', () => {
