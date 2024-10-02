706c9bd56c7ebef02e4af9a343fa76774952c0ef
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-10-02 20:22:03 +0900

    Update benchmarks/performance/isMap.bench.ts
    Co-authored-by: D-Sketon <2055272094@qq.com>

--- benchmarks/performance/isMap.bench.ts
+++ benchmarks/performance/isMap.bench.ts
@@ -1,7 +1,11 @@
 import { bench, describe } from 'vitest';
-import { isMap as isMapToolkit } from 'es-toolkit';
-import { isMap as isMapToolkitCompat } from 'es-toolkit/compat';
-import { isMap as isMapLodash } from 'lodash';
+import { isMap as isMapToolkit_ } from 'es-toolkit';
+import { isMap as isMapToolkitCompat_ } from 'es-toolkit/compat'
+import { isMap as isMapLodash_ } from 'lodash';
+
+const isMapToolkit = isMapToolkit_;
+const isMapToolkitCompat = isMapToolkitCompat_ ;
+const isMapLodash = isMapLodash_;
 
 describe('isMap', () => {
   bench('es-toolkit/isMap', () => {
