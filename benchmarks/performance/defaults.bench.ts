03105eed5913214c5509f82763d823c7c9c2c39d
dayongkr <dayongkr@gmail.com>
2024-10-02 15:59:20 +0900

    Fix bench title

--- benchmarks/performance/defaults.bench.ts
+++ benchmarks/performance/defaults.bench.ts
@@ -1,14 +1,14 @@
 import { bench, describe } from 'vitest';
-import { defaults as defaultsToolkit_ } from 'es-toolkit/compat';
+import { defaults as defaultsToolkitCompat_ } from 'es-toolkit/compat';
 import { defaults as defaultsLodash_ } from 'lodash';
 
-const defaultsToolkit = defaultsToolkit_;
+const defaultsToolkitCompat = defaultsToolkitCompat_;
 const defaultsLodash = defaultsLodash_;
 
 describe('defaults', () => {
-  bench('es-toolkit/defaults', () => {
-    defaultsToolkit({ a: 1 }, { a: 2, b: 2 });
-    defaultsToolkit({ a: 1, b: 2 }, { b: 3 }, { c: 3 });
+  bench('es-toolkit/compat/defaults', () => {
+    defaultsToolkitCompat({ a: 1 }, { a: 2, b: 2 });
+    defaultsToolkitCompat({ a: 1, b: 2 }, { b: 3 }, { c: 3 });
   });
 
   bench('lodash/defaults', () => {
