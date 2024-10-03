3a5978f732a973e79e3eba3509761c765c81c828
dayongkr <dayongkr@gmail.com>
2024-10-04 00:37:34 +0900

    Add constant

---
+++ src/compat/util/constant.spec.ts
@@ -0,0 +1,43 @@
+import { describe, expect, it } from 'vitest';
+import { constant } from './constant';
+import { empties } from '../_internal/empties';
+import { falsey } from '../_internal/falsey';
+import { stubTrue } from '../_internal/stubTrue';
+import * as esToolkit from '../index';
+
+describe('constant', () => {
+  it('should create a function that returns `value`', () => {
+    const object = { a: 1 };
+    const values = Array(2).concat(empties, true, 1, 'a');
+    const _constant = constant(object);
+
+    const results = values.map((value, index) => {
+      if (index < 2) {
+        return index ? _constant.call({}) : _constant();
+      }
+      return _constant(value);
+    });
+
+    expect(esToolkit.every(results, result => result === object));
+  });
+
+  it('should work with falsey values', () => {
+    const expected = falsey.map(stubTrue);
+
+    const actual = falsey.map((value, index) => {
+      const _constant = index ? constant(value) : constant();
+      const result = _constant();
+
+      return result === value || (result !== result && value !== value);
+    });
+
+    expect(actual).toEqual(expected);
+  });
+
+  // Chaning is out of scope for es-toolkit
+
+  // it('should return a wrapped value when chaining', () => {
+  //   const wrapped = _(true).constant();
+  //   expect(wrapped instanceof _);
+  // });
+});
---
+++ src/compat/util/constant.ts
@@ -0,0 +1,17 @@
+/**
+ * Creates a new function that always returns `value`.
+ *
+ * @template T - The type of the value to return.
+ * @param {T} value - The value to return from the new function.
+ * @returns {() => T} Returns the new constant function.
+ *
+ * @example
+ * const object = { a: 1 };
+ * const returnsObject = constant(object);
+ *
+ * returnsObject(); // => { a: 1 }
+ * returnsObject() === object; // => true
+ */
+export function constant<T>(value: T): () => T {
+  return () => value;
+}
