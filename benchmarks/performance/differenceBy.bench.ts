3460dff08b38a72d1473bbd4b3bbbe352c10f5ac
Dongho Kim <dongre2527@gmail.com>
2024-10-03 14:48:32 +0900

    test(differenceBy): Add to benchmark

--- benchmarks/performance/differenceBy.bench.ts
+++ benchmarks/performance/differenceBy.bench.ts
@@ -1,10 +1,15 @@
 import { bench, describe } from 'vitest';
 import { differenceBy as differenceByToolkit } from 'es-toolkit';
+import { differenceBy as differenceByCompatToolkit } from 'es-toolkit/compat';
 import { differenceBy as differenceByLodash } from 'lodash';
 
 describe('differenceBy', () => {
   bench('es-toolkit/differenceBy', () => {
     differenceByToolkit([1.2, 2.3, 3.4], [1.2], Math.floor);
+  });
+
+  bench('es-toolkit/compat/differenceBy', () => {
+    differenceByCompatToolkit([1.2, 2.3, 3.4], [1.2], Math.floor);
   });
 
   bench('lodash/differenceBy', () => {
