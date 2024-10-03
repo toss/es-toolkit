6a954df01a3544bb3e587ca6bb7b6c760e840a56
D-Sketon <2055272094@qq.com>
2024-10-03 21:01:40 +0800

    bench: add missing bench and bump vitest (#655)
    * bench: add missing bench and bump vitest
    
    * make lint happy

--- benchmarks/performance/eq.bench.ts
+++ benchmarks/performance/eq.bench.ts
@@ -6,27 +6,19 @@ const eqToolkitCompat = eqToolkitCompat_;
 const eqLodash = eqLodash_;
 
 describe('eq', () => {
-  bench(
-    'es-toolkit/compat/eq',
-    () => {
-      eqToolkitCompat(NaN, NaN);
-      eqToolkitCompat(NaN, 0);
-      eqToolkitCompat(0, NaN);
-      eqToolkitCompat(0, -0);
-      eqToolkitCompat('abc', 'abc');
-    },
-    { time: 100 } // We need to decrease the time. because vitest throws an error if the time is too long and tested function is too fast.
-  );
+  bench('es-toolkit/compat/eq', () => {
+    eqToolkitCompat(NaN, NaN);
+    eqToolkitCompat(NaN, 0);
+    eqToolkitCompat(0, NaN);
+    eqToolkitCompat(0, -0);
+    eqToolkitCompat('abc', 'abc');
+  });
 
-  bench(
-    'lodash/eq',
-    () => {
-      eqLodash(NaN, NaN);
-      eqLodash(NaN, 0);
-      eqLodash(0, NaN);
-      eqLodash(0, -0);
-      eqLodash('abc', 'abc');
-    },
-    { time: 100 }
-  );
+  bench('lodash/eq', () => {
+    eqLodash(NaN, NaN);
+    eqLodash(NaN, 0);
+    eqLodash(0, NaN);
+    eqLodash(0, -0);
+    eqLodash('abc', 'abc');
+  });
 });
--- benchmarks/performance/toFilled.bench.ts
+++ benchmarks/performance/toFilled.bench.ts
@@ -24,3 +24,15 @@ describe('fill function performance with custom start and end', () => {
     fillLodash([1, 2, 3, 4, 5], '*', 1, 3);
   });
 });
+
+describe('fill function performance with large array', () => {
+  const largeArray = Array.from({ length: 10000 }, (_, index) => index);
+
+  bench('es-toolkit/toFilled', () => {
+    toFilledToolkit(largeArray, '*');
+  });
+
+  bench('lodash/fill', () => {
+    fillLodash(largeArray, '*');
+  });
+});
