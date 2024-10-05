42a4b9424b9e4d5e6bc6798aa771a768819b35ef
Dayong Lee <dayongkr@gmail.com>
2024-10-05 22:16:40 +0900

    feat(constant): add `constant` in compat layer (#657)

--- src/compat/index.ts
+++ src/compat/index.ts
@@ -142,6 +142,7 @@ export { parseInt } from './math/parseInt.ts';
 export { inRange } from './math/inRange.ts';
 export { random } from './math/random.ts';
 
+export { constant } from './util/constant.ts';
 export { toPath } from './util/toPath.ts';
 export { toString } from './util/toString.ts';
 export { toNumber } from './util/toNumber.ts';
