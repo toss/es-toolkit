ba07536b343ca69b553317be48d7bb2df72adae3
dayongkr <dayongkr@gmail.com>
2024-10-07 22:59:37 +0900

    Add escape

--- benchmarks/performance/escape.bench.ts
+++ benchmarks/performance/escape.bench.ts
@@ -1,25 +1,38 @@
 import { bench, describe } from 'vitest';
 import { escape as escapeToolkit_ } from 'es-toolkit';
+import { escape as escapeToolkitCompat_ } from 'es-toolkit/compat';
 import { escape as escapeLodash_ } from 'lodash';
 
 const escapeToolkit = escapeToolkit_;
+const escapeToolkitCompat = escapeToolkitCompat_;
 const escapeLodash = escapeLodash_;
 
 describe('escape', () => {
-  const longString = 'fred, barney, & pebbles'.repeat(100);
   bench('es-toolkit/escape', () => {
     escapeToolkit('fred, barney, & pebbles');
+  });
+
+  bench('es-toolkit/compat/escape', () => {
+    escapeToolkitCompat('fred, barney, & pebbles');
   });
 
   bench('lodash/escape', () => {
     escapeLodash('fred, barney, & pebbles');
   });
+});
 
-  bench('es-toolkit/escape - long string', () => {
+describe('escape (long string)', () => {
+  const longString = 'fred, barney, & pebbles'.repeat(100);
+
+  bench('es-toolkit/escape', () => {
     escapeToolkit(longString);
   });
 
-  bench('lodash/escape long string', () => {
+  bench('es-toolkit/compat/escape', () => {
+    escapeToolkitCompat(longString);
+  });
+
+  bench('lodash/escape', () => {
     escapeLodash(longString);
   });
 });
---
+++ src/compat/string/escape.spec.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+import { escape, unescape } from '../index';
+
+describe('escape', () => {
+  let escaped = '&amp;&lt;&gt;&quot;&#39;/';
+  let unescaped = '&<>"\'/';
+
+  escaped += escaped;
+  unescaped += unescaped;
+
+  it('should escape values', () => {
+    expect(escape(unescaped)).toBe(escaped);
+  });
+
+  it('should handle strings with nothing to escape', () => {
+    expect(escape('abc')).toBe('abc');
+  });
+
+  it('should escape the same characters unescaped by `_.unescape`', () => {
+    expect(escape(unescape(escaped))).toBe(escaped);
+  });
+
+  ['`', '/'].forEach(chr => {
+    it(`should not escape the "${chr}" character`, () => {
+      expect(escape(chr)).toBe(chr);
+    });
+  });
+});
