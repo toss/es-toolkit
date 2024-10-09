31d8b1538092511902db87c9a6a75f26cecf7924
D-Sketon <2055272094@qq.com>
2024-10-09 23:40:07 +0800

    bench

--- benchmarks/performance/takeRight.bench.ts
+++ benchmarks/performance/takeRight.bench.ts
@@ -1,13 +1,19 @@
 import { bench, describe } from 'vitest';
 import { takeRight as takeRightToolkit_ } from 'es-toolkit';
+import { takeRight as takeRightToolkitCompat_ } from 'es-toolkit/compat';
 import { takeRight as takeRightLodash_ } from 'lodash';
 
 const takeRightToolkit = takeRightToolkit_;
+const takeRightToolkitCompat = takeRightToolkitCompat_;
 const takeRightLodash = takeRightLodash_;
 
 describe('takeRight', () => {
   bench('es-toolkit/takeRight', () => {
     takeRightToolkit([1, 2, 3, 4], 2);
+  });
+
+  bench('es-toolkit/compat/takeRight', () => {
+    takeRightToolkitCompat([1, 2, 3, 4], 2);
   });
 
   bench('lodash/takeRight', () => {
@@ -20,6 +26,10 @@ describe('takeRight/largeArray', () => {
 
   bench('es-toolkit/takeRight', () => {
     takeRightToolkit(largeArray, 100);
+  });
+
+  bench('es-toolkit/compat/takeRight', () => {
+    takeRightToolkitCompat(largeArray, 100);
   });
 
   bench('lodash/takeRight', () => {
