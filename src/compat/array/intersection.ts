7fc7d18d03e45f1a0fe4e7fc262e244f511dbd98
Sojin Park <raon0211@gmail.com>
2024-10-13 22:21:41 +0900

    Update intersection.ts

--- src/compat/array/intersection.ts
+++ src/compat/array/intersection.ts
@@ -3,21 +3,22 @@ import { intersection as intersectionToolkit } from '../../array/intersection';
 import { isArrayLikeObject } from '../predicate/isArrayLikeObject';
 
 export function intersection<T>(...arrays: Array<ArrayLike<T> | null | undefined>): T[] {
-  if (arrays.length === 0 || !isArrayLikeObject(arrays[0])) {
+  if (arrays.length === 0) {
     return [];
   }
 
-  const normalizedArrays = arrays.map(array => {
-    if (!isArrayLikeObject(array)) {
+  if (!isArrayLikeObject(arrays[0])) {
+    return [];
+  }
+
+  let result: T[] = uniq(Array.from(arrays[0]));
+  
+  for (let i = 1; i < normalizedArrays.length; i++) {
+    if (!isArrayLikeObject(arrays[i])) {
       return [];
     }
-
-    return Array.from(array);
-  });
-
-  let result: T[] = uniq(normalizedArrays[0]);
-  for (let i = 1; i < normalizedArrays.length; i++) {
-    result = intersectionToolkit(result, normalizedArrays[i]);
+    
+    result = intersectionToolkit(result, Array.from(arrays[i]));
   }
 
   return result;
