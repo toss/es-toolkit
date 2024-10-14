f4c81ad642da1db321021521291bd88ef6d865ad
dayongkr <dayongkr@gmail.com>
2024-10-14 14:28:48 +0900

    Add error message and get target logic

--- .scripts/bench-runner.mjs
+++ .scripts/bench-runner.mjs
@@ -1,11 +1,14 @@
 import { execSync } from 'node:child_process';
-import { existsSync, rmSync } from 'node:fs';
+import { existsSync, readdirSync, rmSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { rollup } from 'rollup';
 import aliasPlugin from '@rollup/plugin-alias';
 import resolvePlugin from '@rollup/plugin-node-resolve';
 import tsPlugin from '@rollup/plugin-typescript';
 import { entrypoints, libBuildOptions } from '../rollup.config.mjs';
+
+const includeStrings = process.argv.slice(2);
+assert(includeStrings.length > 0, 'Please provide at least one include string. (e.g. `yarn bench chunk`)');
 
 const OUTPUT_DIR = '.bench-bundle';
 const dirPath = resolve(import.meta.dirname, OUTPUT_DIR);
@@ -27,43 +30,57 @@ await time('Build es-toolkit', async () => {
   await toolkitBundle.close();
 });
 
-const output = await time('Build benchmark', async () => {
-  // Build the benchmark file
-  const benchBundle = await rollup({
-    input: 'benchmarks/performance/bench.ts',
-    plugins: [
-      aliasPlugin({
-        entries: [{ find: /^es-toolkit/, replacement: `../../${OUTPUT_DIR}` }], // Replace the es-toolkit import with the pre-built.
-      }),
-      tsPlugin({
-        include: ['**/*.ts'],
-        exclude: ['**/*.spec.ts'],
-        compilerOptions: {
-          sourceMap: false,
-          inlineSources: false,
-          removeComments: true,
-          declaration: false,
-        },
-      }),
-      resolvePlugin(),
-    ],
-    logLevel: 'silent',
-  });
-  const { output } = await benchBundle.write({
-    format: 'esm',
-    dir: OUTPUT_DIR,
-    entryFileNames: '[name].mjs',
-    generatedCode: 'es2015',
-  });
-  await benchBundle.close();
-  return output;
-});
+const benchmarkDir = resolve(process.cwd(), 'benchmarks/performance');
+const allBenchmarkFiles = readdirSync(benchmarkDir);
 
-// Run the benchmark
-await time('Benchmark', () => {
-  const bulitBenchFile = resolve(OUTPUT_DIR, output[0].fileName);
-  execSync(`node ${bulitBenchFile}`, { stdio: 'inherit' });
-});
+for (const includeString of includeStrings) {
+  const targetBenchFiles = allBenchmarkFiles.filter(f => f.includes(includeString));
+
+  if (targetBenchFiles.length === 0) {
+    console.warn(`No benchmark files found for "${includeString}".`);
+    continue;
+  }
+
+  for (const file of targetBenchFiles) {
+    const output = await time('Build benchmark', async () => {
+      // Build the benchmark file
+      const benchBundle = await rollup({
+        input: resolve(benchmarkDir, file),
+        plugins: [
+          aliasPlugin({
+            entries: [{ find: /^es-toolkit/, replacement: `../../${OUTPUT_DIR}` }], // Replace the es-toolkit import with the pre-built.
+          }),
+          tsPlugin({
+            include: ['**/*.ts'],
+            exclude: ['**/*.spec.ts'],
+            compilerOptions: {
+              sourceMap: false,
+              inlineSources: false,
+              removeComments: true,
+              declaration: false,
+            },
+          }),
+          resolvePlugin(),
+        ],
+        logLevel: 'silent',
+      });
+      const { output } = await benchBundle.write({
+        format: 'esm',
+        dir: OUTPUT_DIR,
+        entryFileNames: '[name].mjs',
+        generatedCode: 'es2015',
+      });
+      await benchBundle.close();
+      return output;
+    });
+
+    // Run the benchmark
+    await time('Benchmark', () => {
+      const bulitBenchFile = resolve(OUTPUT_DIR, output[0].fileName);
+      execSync(`node ${bulitBenchFile}`, { stdio: 'inherit' });
+    });
+  }
+}
 
 async function time(title, fn) {
   console.log(`Running ${title}...`);
@@ -75,4 +92,11 @@ async function time(title, fn) {
   console.log(`${title}: ${((end - start) / 1000).toFixed(2)}s`);
 
   return result;
+}
+
+function assert(condition, message) {
+  if (!condition) {
+    console.error(message);
+    process.exit(1);
+  }
 }
