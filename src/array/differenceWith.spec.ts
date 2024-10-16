157fb867f2d8f17d2a480db9ce345af2f229ef01
kimseonghun <pch468@naver.com>
2024-10-16 16:51:22 +0900

    Test: add a object array case

--- src/array/differenceWith.spec.ts
+++ src/array/differenceWith.spec.ts
@@ -2,7 +2,12 @@ import { describe, expect, it } from 'vitest';
 import { differenceWith } from './differenceWith';
 
 describe('differenceWith', () => {
-  it('should the difference of two arrays using the `areItemsEqual` function', () => {
+  it('should return the difference of two arrays using the `areItemsEqual` function', () => {
     expect(differenceWith([1.2, 2.3, 3.4], [1.2], (x, y) => Math.floor(x) === Math.floor(y))).toEqual([2.3, 3.4]);
+
+    const array1 = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    const array2 = [{ id: 2 }, { id: 4 }];
+
+    expect(differenceWith(array1, array2, (a, b) => a.id === b.id)).toEqual([{ id: 1 }, { id: 3 }]);
   });
 });
