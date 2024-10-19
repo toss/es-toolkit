aaf5dd1cb1e07cf36110d5f3cfdf233e9e10e4d9
D-Sketon <2055272094@qq.com>
2024-10-19 17:01:19 +0800

    fix(drop/dropRight/every/take/takeRight): support guard (#730)
    * fix(drop/dropRight/every/take/takeRight): support guard
    
    * clean
    
    * clean

--- src/compat/array/drop.ts
+++ src/compat/array/drop.ts
@@ -1,5 +1,6 @@
 import { drop as dropToolkit } from '../../array/drop.ts';
 import { isArrayLike } from '../predicate/isArrayLike.ts';
+import { toInteger } from '../util/toInteger.ts';
 
 /**
  * Removes a specified number of elements from the beginning of an array and returns the rest.
@@ -10,6 +11,7 @@ import { isArrayLike } from '../predicate/isArrayLike.ts';
  * @template T - The type of elements in the array.
  * @param {ArrayLike<T> | null | undefined} collection - The array from which to drop elements.
  * @param {number} itemsCount - The number of elements to drop from the beginning of the array.
+ * @param {unknown} [guard] - Enables use as an iteratee for methods like `_.map`.
  * @returns {T[]} A new array with the specified number of elements removed from the start.
  *
  * @example
@@ -17,10 +19,11 @@ import { isArrayLike } from '../predicate/isArrayLike.ts';
  * const result = drop(array, 2);
  * result will be [3, 4, 5] since the first two elements are dropped.
  */
-export function drop<T>(collection: ArrayLike<T> | null | undefined, itemsCount: number): T[] {
+export function drop<T>(collection: ArrayLike<T> | null | undefined, itemsCount: number = 1, guard?: unknown): T[] {
   if (!isArrayLike(collection)) {
     return [];
   }
+  itemsCount = guard ? 1 : toInteger(itemsCount);
 
   return dropToolkit(Array.from(collection), itemsCount);
 }
--- src/compat/array/dropRight.ts
+++ src/compat/array/dropRight.ts
@@ -1,5 +1,6 @@
 import { dropRight as dropRightToolkit } from '../../array/dropRight.ts';
 import { isArrayLike } from '../predicate/isArrayLike.ts';
+import { toInteger } from '../util/toInteger.ts';
 
 /**
  * Removes a specified number of elements from the end of an array and returns the rest.
@@ -10,6 +11,7 @@ import { isArrayLike } from '../predicate/isArrayLike.ts';
  * @template T - The type of elements in the array.
  * @param {ArrayLike<T> | null | undefined} collection - The array from which to drop elements.
  * @param {number} itemsCount - The number of elements to drop from the end of the array.
+ * @param {unknown} [guard] - Enables use as an iteratee for methods like `_.map`.
  * @returns {T[]} A new array with the specified number of elements removed from the end.
  *
  * @example
@@ -17,10 +19,15 @@ import { isArrayLike } from '../predicate/isArrayLike.ts';
  * const result = dropRight(array, 2);
  * // result will be [1, 2, 3] since the last two elements are dropped.
  */
-export function dropRight<T>(collection: ArrayLike<T> | null | undefined, itemsCount: number): T[] {
+export function dropRight<T>(
+  collection: ArrayLike<T> | null | undefined,
+  itemsCount: number = 1,
+  guard?: unknown
+): T[] {
   if (!isArrayLike(collection)) {
     return [];
   }
+  itemsCount = guard ? 1 : toInteger(itemsCount);
 
   return dropRightToolkit(Array.from(collection), itemsCount);
 }
