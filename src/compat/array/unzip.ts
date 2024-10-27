a27d8b7d4cfa7379a70153f95b2552266a8c6ae5
D-Sketon <2055272094@qq.com>
2024-10-27 19:14:01 +0800

    jsdoc

--- src/compat/array/unzip.ts
+++ src/compat/array/unzip.ts
@@ -1,6 +1,19 @@
 import { unzip as unzipToolkit } from '../../array/unzip.ts';
 import { isArrayLikeObject } from '../predicate/isArrayLikeObject.ts';
 
+/**
+ * Gathers elements in the same position in an internal array
+ * from a grouped array of elements and returns them as a new array.
+ *
+ * @template T - The type of elements in the nested array.
+ * @param {T[][] | ArrayLike<ArrayLike<T>> | null | undefined} array - The nested array to unzip.
+ * @returns {T[][]} A new array of unzipped elements.
+ *
+ * @example
+ * const zipped = [['a', true, 1],['b', false, 2]];
+ * const result = unzip(zipped);
+ * // result will be [['a', 'b'], [true, false], [1, 2]]
+ */
 export function unzip<T>(array: T[][] | ArrayLike<ArrayLike<T>> | null | undefined): T[][] {
   if (!isArrayLikeObject(array) || !array.length) {
     return [];
