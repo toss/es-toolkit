d5b66938ad7d2646207dca67ecab10e44d4386e5
dayongkr <dayongkr@gmail.com>
2024-10-30 10:33:07 +0900

    Add JSDoc

--- src/compat/math/sum.ts
+++ src/compat/math/sum.ts
@@ -38,6 +38,22 @@ export function sum(array: ArrayLike<bigint>): bigint;
  */
 export function sum(array: ArrayLike<unknown> | null | undefined): unknown;
 
+/**
+ * Compmutes the sum of the values that are returned by the `iteratee` function.
+ *
+ * It does not coerce values to `number`.
+ *
+ * @param {ArrayLike<unknown> | null | undefined} array - The array to iterate over.
+ * @returns {unknown} Returns the sum.
+ *
+ * @example
+ * sum([1, 2, 3]); // => 6
+ * sum([1n, 2n, 3n]); // => 6n
+ * sum(["1", "2"]); // => "12"
+ * sum([1, undefined, 2]); // => 3
+ * sum(null); // => 0
+ * sum(undefined); // => 0
+ */
 export function sum(array: ArrayLike<unknown> | null | undefined): unknown {
   return sumBy(array);
 }
--- src/compat/math/sumBy.ts
+++ src/compat/math/sumBy.ts
@@ -64,6 +64,24 @@ export function sumBy<T>(array: ArrayLike<T>, iteratee: (value: T) => number): n
  */
 export function sumBy<T>(array: ArrayLike<T>, iteratee: (value: T) => bigint): bigint;
 
+/**
+ * Compmutes the sum of the values that are returned by the `iteratee` function.
+ *
+ * It does not coerce values to `number`.
+ *
+ * @template T - The type of the array elements.
+ * @param {ArrayLike<T> | null | undefined} array - The array to iterate over.
+ * @param {(value: T) => number | bigint} iteratee - The function invoked per iteration.
+ * @returns {unknown} Returns the sum.
+ *
+ * @example
+ * sumBy([1, undefined, 2], value => value); // => 3
+ * sumBy(null); // => 0
+ * sumBy(undefined); // => 0
+ * sumBy([1, 2, 3]); // => 6
+ * sumBy([1n, 2n, 3n]); // => 6n
+ * sumBy([{ a: "1" }, { a: "2" }], object => object.a); // => "12"
+ */
 export function sumBy<T>(array: ArrayLike<T> | null | undefined, iteratee?: (value: T) => number | bigint): unknown {
   if (!array || !array.length) {
     return 0;
