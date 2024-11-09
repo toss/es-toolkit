6d4fe722843c8b263ef38a0bcdd3ffa72629a179
Gyumong <60845910+Gyumong@users.noreply.github.com>
2024-11-09 22:53:33 +0900

    feat(uniqBy): add uniqBy to compat layer (#793)
    * feat(compat): implement array/uniqBy
    
    * feat(compat): uniqBy test code
    
    * feat(compat): uniqBy bench
    
    * fix(compat): enhance uniqBy types and docs
    
    * fix(compat): test code lint

--- src/compat/index.ts
+++ src/compat/index.ts
@@ -64,6 +64,7 @@ export { take } from './array/take.ts';
 export { takeRight } from './array/takeRight.ts';
 export { union } from './array/union.ts';
 export { uniq } from './array/uniq.ts';
+export { uniqBy } from './array/uniqBy.ts';
 export { unzip } from './array/unzip.ts';
 export { without } from './array/without.ts';
 export { zip } from './array/zip.ts';
