32019fa7dadc716cd82188a41ab14be6518bac26
Noel Kim (김민혁) <noelkim@prestolabs.io>
2024-11-14 21:32:08 +0900

    fix(intersectionWith): first array and second array's types can different (#823)

--- src/array/intersectionWith.spec.ts
+++ src/array/intersectionWith.spec.ts
@@ -8,4 +8,22 @@ describe('intersectionWith', () => {
       intersectionWith([{ foo: 1 }, { foo: 2 }], [{ foo: 1 }, { foo: 3 }], (x, y) => x.foo === y.foo)
     ).toStrictEqual([{ foo: 1 }]);
   });
+
+  it('should return the intersection of two arrays have different type', () => {
+    type CSV = { id: number; csv: number };
+    type JSON = { id: number; json: number };
+
+    const array1: CSV[] = [
+      { id: 1, csv: 1 },
+      { id: 2, csv: 1 },
+      { id: 3, csv: 1 },
+    ];
+    const array2: JSON[] = [
+      { id: 2, json: 2 },
+      { id: 4, json: 2 },
+    ];
+
+    const result = intersectionWith(array1, array2, (a, b) => a.id === b.id);
+    expect(result).toEqual([{ id: 2, csv: 1 }]);
+  });
 });
