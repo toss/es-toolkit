fe3e6354bc73768a63d55c83d42e2fe692428b70
Dongho Kim <dongre2527@gmail.com>
2024-11-18 22:15:54 +0900

    refactor(intersectionWith): Update type parameter name for consistency

--- src/array/intersectionWith.ts
+++ src/array/intersectionWith.ts
@@ -6,13 +6,13 @@
  * by the custom equality function. It effectively filters out any elements from the first array that
  * do not have corresponding matches in the second array according to the equality function.
  *
- * @template F - The type of elements in the first array.
- * @template S - The type of elements in the second array.
- * @param {F[]} firstArr - The first array to compare.
- * @param {S[]} secondArr - The second array to compare.
- * @param {(x: F, y: S) => boolean} areItemsEqual - A custom function to determine if two elements are equal.
+ * @template T - The type of elements in the first array.
+ * @template U - The type of elements in the second array.
+ * @param {T[]} firstArr - The first array to compare.
+ * @param {U[]} secondArr - The second array to compare.
+ * @param {(x: T, y: U) => boolean} areItemsEqual - A custom function to determine if two elements are equal.
  * This function takes two arguments, one from each array, and returns `true` if the elements are considered equal, and `false` otherwise.
- * @returns {F[]} A new array containing the elements from the first array that have corresponding matches in the second array according to the custom equality function.
+ * @returns {T[]} A new array containing the elements from the first array that have corresponding matches in the second array according to the custom equality function.
  *
  * @example
  * const array1 = [{ id: 1 }, { id: 2 }, { id: 3 }];
@@ -21,11 +21,11 @@
  * const result = intersectionWith(array1, array2, areItemsEqual);
  * // result will be [{ id: 2 }] since this element has a matching id in both arrays.
  */
-export function intersectionWith<F, S>(
-  firstArr: readonly F[],
-  secondArr: readonly S[],
-  areItemsEqual: (x: F, y: S) => boolean
-): F[] {
+export function intersectionWith<T, U>(
+  firstArr: readonly T[],
+  secondArr: readonly U[],
+  areItemsEqual: (x: T, y: U) => boolean
+): T[] {
   return firstArr.filter(firstItem => {
     return secondArr.some(secondItem => {
       return areItemsEqual(firstItem, secondItem);
