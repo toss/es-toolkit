d11531b8fb469d753490cead62ea5e279d980345
Sojin Park <raon0211@gmail.com>
2024-11-23 21:59:45 +0900

    Move add to compat

--- benchmarks/performance/add.bench.ts
+++ benchmarks/performance/add.bench.ts
@@ -1,21 +1,11 @@
 import { bench, describe } from 'vitest';
-import { add as addToolkit_ } from 'es-toolkit';
 import { add as addToolkitCompat_ } from 'es-toolkit/compat';
 import { add as addLodash_ } from 'lodash';
 
-const addToolkit = addToolkit_;
 const addToolkitCompat = addToolkitCompat_;
 const addLodash = addLodash_;
 
 describe('add function benchmark', () => {
-  bench('es-toolkit/add', () => {
-    addToolkit(2, 3);
-    addToolkit(-1, -5);
-    addToolkit(NaN, 3);
-    addToolkit(3, NaN);
-    addToolkit(NaN, NaN);
-  });
-
   bench('es-toolkit/compat/add', () => {
     addToolkitCompat(2, 3);
     addToolkitCompat(-1, -5);
