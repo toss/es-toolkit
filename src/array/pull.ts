8eabc5e8da724ece71f6af9d4bba85831e7919be
jodasom <jodasom0216@gmail.com>
2024-11-25 22:36:48 +0900

    fix: lint

--- src/array/pull.ts
+++ src/array/pull.ts
@@ -1,12 +1,12 @@
 /**
- * Removes all occurrences of the specified values from an array and returns the modified array.
+ * 지정된 값을 배열에서 모두 제거하고 수정된 배열을 반환합니다.
  *
- * This function mutates the original array.
+ * 이 함수는 원본 배열을 변형합니다.
  *
  * @template T
- * @param {T[]} arr - The array from which elements will be removed.
- * @param {...T} values - The values to remove from the array.
- * @returns {T[]} The modified array with specified values removed.
+ * @param {T[]} arr - 요소를 제거할 배열입니다.
+ * @param {...T} values - 배열에서 제거할 값들입니다.
+ * @returns {T[]} 지정된 값들이 제거된 수정된 배열입니다.
  *
  * @example
  * import { pull } from './pull';
@@ -16,13 +16,13 @@
  * console.log(numbers); // [2, 2]
  */
 export function pull<T>(arr: T[], ...values: T[]): T[] {
-    const valuesSet = new Set(values);
+  const valuesSet = new Set(values);
 
-    for (let i = arr.length - 1; i >= 0; i--) {
-      if (valuesSet.has(arr[i])) {
-        arr.splice(i, 1);
-      }
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (valuesSet.has(arr[i])) {
+      arr.splice(i, 1);
     }
-
-    return arr;
   }
+
+  return arr;
+}
