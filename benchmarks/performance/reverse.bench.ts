18b48f3c012132cfb658b84a04d4738a0994fd07
Sojin Park <raon0211@gmail.com>
2024-12-07 23:34:30 +0900

    update

--- benchmarks/performance/reverse.bench.ts
+++ benchmarks/performance/reverse.bench.ts
@@ -2,16 +2,19 @@ import { bench, describe } from 'vitest';
 import { reverse as reverseCompat_ } from 'es-toolkit/compat';
 import { reverse as reverseLodash_ } from 'lodash';
 
+const reverseCompat = reverseCompat_;
+const reverseLodash = reverseLodash_;
+
 const testArray = Array.from({ length: 1000 }, (_, i) => i);
 
 describe('Reverse function performance tests', () => {
   bench('es-toolkit compat reverse', () => {
     const arrayCopy = [...testArray];
-    reverseCompat_(arrayCopy);
+    reverseCompat(arrayCopy);
   });
 
   bench('lodash reverse', () => {
     const arrayCopy = [...testArray];
-    reverseLodash_(arrayCopy);
+    reverseLodash(arrayCopy);
   });
 });
