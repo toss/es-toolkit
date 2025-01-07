d55046090cc70a5079eceeee5ceffc7563da7517
hobermin <sonhomin98@naver.com>
2025-01-07 16:46:10 +0900

    fix: CircleCI lint error

--- src/predicate/isFiniteNumber.spec.ts
+++ src/predicate/isFiniteNumber.spec.ts
@@ -3,14 +3,7 @@ import { isFiniteNumber } from './isFiniteNumber';
 
 describe('isFiniteNumber', () => {
   it('should return `true` for finite numbers', () => {
-    const values = [
-      0,
-      42,
-      -1,
-      3.14,
-      Number.MAX_SAFE_INTEGER,
-      Number.MIN_SAFE_INTEGER
-    ];
+    const values = [0, 42, -1, 3.14, Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
     const expected = values.map(() => true);
     const actual = values.map(isFiniteNumber);
 
@@ -18,18 +11,7 @@ describe('isFiniteNumber', () => {
   });
 
   it('should return `false` for non-finite numbers and other types', () => {
-    const values = [
-      Infinity,
-      -Infinity,
-      NaN,
-      '42',
-      null,
-      undefined,
-      {},
-      [],
-      true,
-      false
-    ];
+    const values = [Infinity, -Infinity, NaN, '42', null, undefined, {}, [], true, false];
     const expected = values.map(() => false);
     const actual = values.map(isFiniteNumber);
 
