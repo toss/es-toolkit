f3b8cee39c4882917a5cbd14c8c685709e38e537
seungro <seungro@toss.im>
2025-01-22 16:39:35 +0900

    feat. add fp/partition

---
+++ src/fp/array/partition.spec.ts
@@ -0,0 +1,74 @@
+import { describe, expect, expectTypeOf, it } from 'vitest';
+import { partition } from './partition';
+
+describe('partition', () => {
+  it(
+    "(non-curried) creates two arrays: the first array includes items for which isInTruthy returns true, and the second array includes items for which isInTruthy returns false.",
+    () => {
+      expect(partition([true, true, false], x => x)).toEqual([[true, true], [false]]);
+      expect(
+        partition(
+          [
+            { id: 1, enabled: true },
+            { id: 2, enabled: false },
+            { id: 3, enabled: false },
+            { id: 4, enabled: true },
+          ],
+          x => x.enabled
+        )
+      ).toEqual([
+        [
+          { id: 1, enabled: true },
+          { id: 4, enabled: true },
+        ],
+        [
+          { id: 2, enabled: false },
+          { id: 3, enabled: false },
+        ],
+      ]);
+    }
+  );
+
+  it("(curried) creates two arrays: the first array includes items for which isInTruthy returns true, and the second array includes items for which isInTruthy returns false.", () => {
+    expect(partition(x => x)([true, true, false])).toEqual([[true, true], [false]]);
+    expect(
+      partition(x => x.enabled)([
+        { id: 1, enabled: true },
+        { id: 2, enabled: false },
+        { id: 3, enabled: false },
+        { id: 4, enabled: true },
+      ])
+    ).toEqual([
+      [
+        { id: 1, enabled: true },
+        { id: 4, enabled: true },
+      ],
+      [
+        { id: 2, enabled: false },
+        { id: 3, enabled: false },
+      ],
+    ]);
+  });
+
+  it("(non-curried) should correctly infer the type of a narrow array", () => {
+    const arr = [1, 2, 3, 4, 5] as const;
+    const [evens, odds] = partition(arr, num => num % 2 === 0);
+
+    expect(evens).toEqual([2, 4]);
+    expect(odds).toEqual([1, 3, 5]);
+
+    expectTypeOf(evens).toEqualTypeOf<Array<1 | 2 | 3 | 4 | 5>>();
+    expectTypeOf(odds).toEqualTypeOf<Array<1 | 2 | 3 | 4 | 5>>();
+  });
+
+  it("(curried) should correctly infer the type of a narrow array", () => {
+    const arr = [1, 2, 3, 4, 5] as const;
+    const [evens, odds] = partition(num => num % 2 === 0)(arr);
+
+    expect(evens).toEqual([2, 4]);
+    expect(odds).toEqual([1, 3, 5]);
+
+    expectTypeOf(evens).toEqualTypeOf<Array<1 | 2 | 3 | 4 | 5>>();
+    expectTypeOf(odds).toEqualTypeOf<Array<1 | 2 | 3 | 4 | 5>>();
+  });
+});
---
+++ src/fp/array/partition.ts
@@ -0,0 +1,60 @@
+import { partition as partitionToolkit } from '../../array/partition';
+
+/**
+ * Splits an array into two groups based on a predicate function.
+ *
+ * This function takes an array and a predicate function. It returns a tuple of two arrays:
+ * the first array contains elements for which the predicate function returns true, and
+ * the second array contains elements for which the predicate function returns false.
+ *
+ * @template T - The type of elements in the array.
+ * @param {T[]} arr - The array to partition.
+ * @param {(value: T) => boolean} isInTruthy - A predicate function that determines
+ * whether an element should be placed in the truthy array. The function is called with each
+ * element of the array.
+ * @returns {[T[], T[]]} A tuple containing two arrays: the first array contains elements for
+ * which the predicate returned true, and the second array contains elements for which the
+ * predicate returned false.
+ *
+ * @example
+ * const array = [1, 2, 3, 4, 5];
+ * const isEven = x => x % 2 === 0;
+ * const [even, odd] = partition(array, isEven);
+ * // even will be [2, 4], and odd will be [1, 3, 5]
+ */
+export function partition<T>(arr: readonly T[], isInTruthy: (value: T) => boolean): [truthy: T[], falsy: T[]];
+
+/**
+ * Splits an array into two groups based on a predicate function.
+ *
+ * This function takes an array and a predicate function. It returns a tuple of two arrays:
+ * the first array contains elements for which the predicate function returns true, and
+ * the second array contains elements for which the predicate function returns false.
+ *
+ * @template T - The type of elements in the array.
+ * @param {(value: T) => boolean} isInTruthy - A predicate function that determines
+ * whether an element should be placed in the truthy array. The function is called with each
+ * element of the array.
+ * @returns {(arr: T[]) => [T[], T[]]} A function that receive the array to partition as argument and returns a tuple containing two arrays: the first array contains elements for.
+ * which the predicate returned true, and the second array contains elements for which the
+ * predicate returned false.
+ *
+ * @example
+ * const array = [1, 2, 3, 4, 5];
+ * const isEven = x => x % 2 === 0;
+ * const [even, odd] = partition(isEven)(array);
+ * // even will be [2, 4], and odd will be [1, 3, 5]
+ */
+export function partition<T>(isInTruthy: (value: T) => boolean): (arr: readonly T[]) => [truthy: T[], falsy: T[]];
+
+export function partition<T>(
+  arrOrIsInTruthy: readonly T[] | ((value: T) => boolean),
+  isInTruthy?: (value: T) => boolean
+) {
+  if (isInTruthy == null) {
+    return (arr: readonly T[]) => partition(arr, arrOrIsInTruthy as (value: T) => boolean);
+  }
+
+  const arr = arrOrIsInTruthy as readonly T[];
+  return partitionToolkit(arr, isInTruthy);
+}
