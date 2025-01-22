23e9ae24956a3c53b33ea0abd54464da8d83c95a
seungro <seungro@toss.im>
2025-01-22 16:43:47 +0900

    fix. fix test types

--- src/fp/array/partition.spec.ts
+++ src/fp/array/partition.spec.ts
@@ -30,9 +30,9 @@ describe('partition', () => {
   );
 
   it("(curried) creates two arrays: the first array includes items for which isInTruthy returns true, and the second array includes items for which isInTruthy returns false.", () => {
-    expect(partition(x => x)([true, true, false])).toEqual([[true, true], [false]]);
+    expect(partition<boolean>(x => x)([true, true, false])).toEqual([[true, true], [false]]);
     expect(
-      partition(x => x.enabled)([
+      partition<{ id: number; enabled: boolean }>(x => x.enabled)([
         { id: 1, enabled: true },
         { id: 2, enabled: false },
         { id: 3, enabled: false },
@@ -63,7 +63,7 @@ describe('partition', () => {
 
   it("(curried) should correctly infer the type of a narrow array", () => {
     const arr = [1, 2, 3, 4, 5] as const;
-    const [evens, odds] = partition(num => num % 2 === 0)(arr);
+    const [evens, odds] = partition<typeof arr[number]>(num => num % 2 === 0)(arr);
 
     expect(evens).toEqual([2, 4]);
     expect(odds).toEqual([1, 3, 5]);
