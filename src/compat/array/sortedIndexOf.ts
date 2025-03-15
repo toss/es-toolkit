ee9e8398fc9c0aecc4dcfe183e656b42299d0174
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-03-15 20:52:07 +0900

    Update src/compat/array/sortedIndexOf.ts
    Co-authored-by: Dayong Lee <dayong@toss.im>

--- src/compat/array/sortedIndexOf.ts
+++ src/compat/array/sortedIndexOf.ts
@@ -5,8 +5,8 @@ import { sortedIndex } from './sortedIndex';
  * @param {ArrayLike<T> | null | undefined} array The sorted array to inspect.
  * @param {T} value The value to search for.
  * @returns {number} Returns the index of the matched value, else -1.
- * @example
  *
+ * @example
  * sortedIndexOf([4,5,5,5,6], 5) => 1
  * sortedIndexOf([1.1, 2.2, 3.3], 2.2) => 1
  *
