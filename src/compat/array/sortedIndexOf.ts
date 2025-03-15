f60965ee9ec5e313aa15b8f0bca84ccd6ec027a7
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-03-15 20:51:51 +0900

    Update src/compat/array/sortedIndexOf.ts
    Co-authored-by: Dayong Lee <dayong@toss.im>

--- src/compat/array/sortedIndexOf.ts
+++ src/compat/array/sortedIndexOf.ts
@@ -1,7 +1,7 @@
 import { sortedIndex } from './sortedIndex';
 
 /**
- * This method is like `indexOf` but performs a binary search on a sorted array.
+ * This method is like `indexOf` but performs a binary search on a sorted `array`.
  * @param {ArrayLike<T> | null | undefined} array The sorted array to inspect.
  * @param {T} value The value to search for.
  * @returns {number} Returns the index of the matched value, else -1.
