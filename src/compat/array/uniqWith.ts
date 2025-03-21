25d10cd679c6b107794460a51702707f796e8735
JRJR519 <kkwjdfo@gmail.com>
2025-03-22 06:19:10 +0900

    docs(uniqWith): jsdoc으로 설명 추가

--- src/compat/array/uniqWith.ts
+++ src/compat/array/uniqWith.ts
@@ -4,6 +4,29 @@ import { isArrayLike } from '../predicate/isArrayLike.ts';
 
 type Comparator<T> = (a: T, b: T) => boolean;
 
+/**
+ * This method is like `uniq`, except that it accepts a `comparator` which is used to determine the equality of elements.
+ *
+ * It creates a duplicate-free version of an array, in which only the first occurrence of each element is kept.
+ * If a `comparator` is provided, it will be invoked with two arguments: `(arrVal, othVal)` to compare elements.
+ * If no comparator is provided, shallow equality is used.
+ *
+ * The order of result values is determined by the order they appear in the input array.
+ *
+ * @template T - The type of elements in the array.
+ * @param {ArrayLike<T> | null | undefined} arr  - The array to process.
+ * @param {Comparator<T>} [comparator] - Optional function to compare elements for equality.
+ * @returns {T[]} A new array with only unique values based on the comparator.
+ *
+ * @example
+ * const array = [1, 2, 2, 3];
+ * const result = uniqWith(array);
+ * // result will be [1, 2, 3]
+ *
+ * const array = [1, 2, 3];
+ * const result = uniqWith(array, (a, b) => a % 2 === b % 2)
+ * // result will be [1, 2]
+ */
 export function uniqWith<T>(arr: ArrayLike<T> | null | undefined, comparator?: Comparator<T>): T[] {
   if (!isArrayLike(arr)) {
     return [];
