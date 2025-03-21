33d1a67a49ca10fd92b4e6c1b00c6330e951e555
JRJR519 <kkwjdfo@gmail.com>
2025-03-22 06:04:25 +0900

    test(uniqWith): test case 2까지 반영

--- src/compat/array/uniqWith.spec.ts
+++ src/compat/array/uniqWith.spec.ts
@@ -5,6 +5,9 @@ import { isEven } from '../_internal/isEven';
 import { LARGE_ARRAY_SIZE } from '../_internal/LARGE_ARRAY_SIZE';
 
 describe('uniqWith', () => {
+  const objects = [{ a: 2 }, { a: 3 }, { a: 1 }, { a: 2 }, { a: 3 }, { a: 1 }];
+
+  // test case #1
   it('should work with a `comparator`', () => {
     const objects = [
       { x: 1, y: 2 },
@@ -27,5 +30,117 @@ describe('uniqWith', () => {
     );
 
     expect(actual).toEqual(expected);
+  });
+
+  // test case #2
+  it(`should return unique values of an unsorted array`, () => {
+    const array = [2, 1, 2];
+    expect(uniqWith(array)).toEqual([2, 1]);
+  });
+
+  it(`should return unique values of a sorted array`, () => {
+    const array = [1, 2, 2];
+    expect(uniqWith(array)).toEqual([1, 2]);
+  });
+
+  it(`should treat object instances as unique`, () => {
+    expect(uniqWith(objects)).toEqual(objects);
+  });
+
+  it(`should treat \`-0\` as \`0\``, () => {
+    const actual = lodashStable.map(uniqWith([-0, 0]), lodashStable.toString);
+    expect(actual).toEqual(['0']);
+  });
+
+  it(`should match \`NaN\``, () => {
+    expect(uniqWith([NaN, NaN])).toEqual([NaN]);
+  });
+
+  it(`should work with large arrays`, () => {
+    const largeArray = [];
+    const expected = [0, {}, 'a'];
+    const count = Math.ceil(LARGE_ARRAY_SIZE / expected.length);
+
+    lodashStable.each(expected, value => {
+      lodashStable.times(count, () => {
+        largeArray.push(value);
+      });
+    });
+
+    expect(uniqWith(largeArray)).toEqual(expected);
+  });
+
+  it(`should work with large arrays of \`-0\` as \`0\``, () => {
+    const largeArray = lodashStable.times(LARGE_ARRAY_SIZE, index => (isEven(index) ? -0 : 0));
+
+    const actual = lodashStable.map(uniqWith(largeArray), lodashStable.toString);
+    expect(actual).toEqual(['0']);
+  });
+
+  it(`should work with large arrays of boolean, \`NaN\`, and nullish values`, () => {
+    const largeArray = [];
+    const expected = [null, undefined, false, true, NaN];
+    const count = Math.ceil(LARGE_ARRAY_SIZE / expected.length);
+
+    lodashStable.each(expected, value => {
+      lodashStable.times(count, () => {
+        largeArray.push(value);
+      });
+    });
+
+    expect(uniqWith(largeArray)).toEqual(expected);
+  });
+
+  it(`should work with large arrays of symbols`, () => {
+    if (Symbol) {
+      const largeArray = lodashStable.times(LARGE_ARRAY_SIZE, Symbol);
+      expect(uniqWith(largeArray)).toEqual(largeArray);
+    }
+  });
+
+  it(`should work with large arrays of well-known symbols`, () => {
+    // See http://www.ecma-international.org/ecma-262/6.0/#sec-well-known-symbols.
+    if (Symbol) {
+      let expected = [
+        Symbol.hasInstance,
+        Symbol.isConcatSpreadable,
+        Symbol.iterator,
+        Symbol.match,
+        Symbol.replace,
+        Symbol.search,
+        Symbol.species,
+        Symbol.split,
+        Symbol.toPrimitive,
+        Symbol.toStringTag,
+        Symbol.unscopables,
+      ];
+
+      const largeArray = [];
+      const count = Math.ceil(LARGE_ARRAY_SIZE / expected.length);
+
+      expected = lodashStable.map(expected, symbol => symbol || {});
+
+      lodashStable.each(expected, value => {
+        lodashStable.times(count, () => {
+          largeArray.push(value);
+        });
+      });
+
+      expect(uniqWith(largeArray)).toEqual(expected);
+    }
+  });
+
+  it(`should distinguish between numbers and numeric strings`, () => {
+    const largeArray = [];
+    const expected = ['2', 2, Object('2'), Object(2)];
+    const count = Math.ceil(LARGE_ARRAY_SIZE / expected.length);
+
+    lodashStable.each(expected, value => {
+      lodashStable.times(count, () => {
+        largeArray.push(value);
+      });
+    });
+
+    expect(uniqWith(largeArray)).toEqual(expected);
   });
 });
