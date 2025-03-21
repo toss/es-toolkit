7dce093c0b35a6004b83d4f3e631f2e6b0ee3128
JRJR519 <kkwjdfo@gmail.com>
2025-03-22 06:04:14 +0900

    feat(uniqWith): comparator 없을 때, uniq 호출하도록 수정

--- src/compat/array/uniqWith.ts
+++ src/compat/array/uniqWith.ts
@@ -1,11 +1,13 @@
 import { uniqWith as uniqWithToolkit } from '../../array/uniqWith.ts';
+import { uniq as uniqToolkit } from '../array/uniq.ts';
 import { isArrayLike } from '../predicate/isArrayLike.ts';
 
 type Comparator<T> = (a: T, b: T) => boolean;
 
-export function uniqWith<T>(arr: ArrayLike<T> | null | undefined, comparator: Comparator<T>): T[] {
+export function uniqWith<T>(arr: ArrayLike<T> | null | undefined, comparator?: Comparator<T>): T[] {
   if (!isArrayLike(arr)) {
     return [];
   }
-  return uniqWithToolkit(Array.from(arr), comparator);
+
+  return typeof comparator === 'function' ? uniqWithToolkit(Array.from(arr), comparator) : uniqToolkit(Array.from(arr));
 }
