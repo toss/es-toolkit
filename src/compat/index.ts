04bbb623f115db261fd5bcd12a09cfe41e696edd
Dongho Kim <70563791+mass2527@users.noreply.github.com>
2025-04-16 21:05:20 +0900

    feat(overSome): Add `overSome` to compat (#1069)
    * feat: add `stubNull`
    
    * feat(overSome): add `overSome` to compat
    
    * refactor(overSome): rename `iteratee` to `createIteratee`
    
    ---------
    
    Co-authored-by: Dayong Lee <dayong@tossbank.com>
    Co-authored-by: Sojin Park <raon0211@toss.im>

--- src/compat/index.ts
+++ src/compat/index.ts
@@ -257,6 +257,7 @@ export { method } from './util/method.ts';
 export { methodOf } from './util/methodOf.ts';
 export { now } from './util/now.ts';
 export { over } from './util/over.ts';
+export { overSome } from './util/overSome.ts';
 export { stubArray } from './util/stubArray.ts';
 export { stubFalse } from './util/stubFalse.ts';
 export { stubObject } from './util/stubObject.ts';
