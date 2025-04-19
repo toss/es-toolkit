43e6695786148d2c6a8c5a2eac1d32abb3acc65e
Dongho Kim <70563791+mass2527@users.noreply.github.com>
2025-04-19 16:31:48 +0900

    feat(forOwn): Implement forOwn (#955)
    * feat(forOwn): Implement forOwn
    
    * fix(forOwn): resolve lint error
    
    * style(forOwn): apply prettier
    
    * Update forOwn.ts
    
    ---------
    
    Co-authored-by: Dayong Lee <dayong@tossbank.com>
    Co-authored-by: Dayong Lee <dayong@toss.im>

--- src/compat/index.ts
+++ src/compat/index.ts
@@ -154,6 +154,7 @@ export { cloneDeepWith } from './object/cloneDeepWith.ts';
 export { create } from './object/create.ts';
 export { defaults } from './object/defaults.ts';
 export { findKey } from './object/findKey.ts';
+export { forOwn } from './object/forOwn.ts';
 export { fromPairs } from './object/fromPairs.ts';
 export { functions } from './object/functions.ts';
 export { functionsIn } from './object/functionsIn.ts';
