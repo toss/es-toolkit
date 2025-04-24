9fe955b9b20ada7bc8faffedf94d1eae80f2738e
Sojin Park <raon0211@toss.im>
2025-04-24 17:25:48 +0900

    sec

--- src/compat/toolkit.ts
+++
@@ -1,9 +0,0 @@
-import * as _toolkit from "./index.ts";
-
-type Toolkit = typeof _toolkit;
-
-export const toolkit: Toolkit & ((value: any) => any) = (value: any): any => {
-  return value;
-};
-
-Object.assign(toolkit, _toolkit);
