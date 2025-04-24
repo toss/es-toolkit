7af6da3f82f2e9e2ba51d90efbb26fd01f77fc77
Sojin Park <raon0211@toss.im>
2025-04-24 20:50:04 +0900

    feat(groupBy): Implement groupBy (#1128)
    * groupBy
    
    * groupBy
    
    * groupBy
    
    * Rename
    
    * fix
    
    * fix compat
    
    * .ts

--- src/compat/compat.ts
+++ src/compat/compat.ts
@@ -23,6 +23,8 @@ export { flattenDeep } from './array/flattenDeep.ts';
 export { flattenDepth } from './array/flattenDepth.ts';
 export { forEach as each } from './array/forEach.ts';
 export { forEach } from './array/forEach.ts';
+export { forEachRight } from './array/forEachRight.ts';
+export { groupBy } from './array/groupBy.ts';
 export { head } from './array/head.ts';
 export { head as first } from './array/head.ts';
 export { includes } from './array/includes.ts';
