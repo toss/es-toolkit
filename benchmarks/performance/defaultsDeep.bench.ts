43ddbb7ffc84a95db5656ed2832b914cf8755a37
hwibaski <hwibaski@gmail.com>
2025-05-12 10:19:05 +0900

    test: vitest 캐시로 인해 벤치마크 정상 동작하지 않았던 이슈해결

--- benchmarks/performance/defaultsDeep.bench.ts
+++ benchmarks/performance/defaultsDeep.bench.ts
@@ -1,17 +1,11 @@
 import { bench, describe } from 'vitest';
 import { defaultsDeep as defaultsDeepToolkitCompat_ } from 'es-toolkit/compat';
 import { defaultsDeep as defaultsDeepLodash_ } from 'lodash';
-import { defaultsDeep } from '../../src/compat/object/defaultsDeep';
 
 const defaultsDeepToolkitCompat = defaultsDeepToolkitCompat_;
 const defaultsDeepLodash = defaultsDeepLodash_;
 
 describe('defaultsDeep', () => {
-  bench('es-toolkit/compat/defaultsDeep (direct import)', () => {
-    defaultsDeep({ a: 1 }, { a: 2, b: 2 });
-    defaultsDeep({ a: 1, b: 2 }, { b: 3 }, { c: 3 });
-  });
-
   bench('es-toolkit/compat/defaultsDeep ', () => {
     defaultsDeepToolkitCompat({ a: 1 }, { a: 2, b: 2 });
     defaultsDeepToolkitCompat({ a: 1, b: 2 }, { b: 3 }, { c: 3 });
