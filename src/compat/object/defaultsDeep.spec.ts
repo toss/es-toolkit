9a3d6955da69984839f1044b6fd9038c69501626
Sojin Park <raon0211@gmail.com>
2025-05-17 11:54:02 +0900

    fix spec

--- src/compat/object/defaultsDeep.spec.ts
+++ src/compat/object/defaultsDeep.spec.ts
@@ -35,15 +35,15 @@ describe('defaultsDeep', () => {
     const source = { a: { b: /y/ } };
     const actual = defaultsDeep(object, source);
 
-    expect((actual as any).a.b).toEqual(/x/);
+    expect(actual.a.b).toEqual(/x/);
   });
 
   it('should not convert function properties to objects', () => {
-    let actual = defaultsDeep({}, { a: noop });
-    expect((actual as any).a).toBe(noop);
+    const actual = defaultsDeep({}, { a: noop });
+    expect(actual.a).toBe(noop);
 
-    actual = defaultsDeep({}, { a: { b: noop } }) as any;
-    expect((actual as any).a.b).toBe(noop);
+    const actual2 = defaultsDeep({}, { a: { b: noop } });
+    expect(actual2.a.b).toBe(noop);
   });
 
   it('should overwrite `undefined` values', () => {
@@ -79,7 +79,7 @@ describe('defaultsDeep', () => {
 
     const actual = defaultsDeep(object, source);
 
-    expect((actual as any).bar.b).toBe((actual as any).foo.b);
+    expect((actual as any).bar.b).toBe(actual.foo.b);
     expect((actual as any).foo.b.c.d).toBe((actual as any).foo.b.c.d.foo.b.c.d);
   });
 
@@ -95,7 +95,7 @@ describe('defaultsDeep', () => {
 
   it('should not attempt a merge of a string into an array', () => {
     const actual = defaultsDeep({ a: ['abc'] }, { a: 'abc' });
-    expect((actual as any).a).toEqual(['abc']);
+    expect(actual.a).toEqual(['abc']);
   });
 
   it('should handle null or undefined sources', () => {
@@ -111,5 +111,115 @@ describe('defaultsDeep', () => {
 
     const result3 = defaultsDeep(target2, { d: 4 }, { d: 4 }, { d: 4 });
     expect(result3).toEqual({ b: 2, d: 4 });
+  });
+
+  it('should not indirectly merge `Object` properties', () => {
+    defaultsDeep({}, { constructor: { a: 1 } });
+
+    const actual = 'a' in Object;
+    delete (Object as any).a;
+
+    expect(actual).toBe(false);
+  });
+
+  describe('defaultsDeep edge cases', () => {
+    it('should not assign values that are the same as their destinations', () => {
+      ['a', ['a'], { a: 1 }, NaN].forEach(value => {
+        const object = {};
+        let pass = true;
+
+        Object.defineProperty(object, 'a', {
+          configurable: true,
+          enumerable: true,
+          get: () => value,
+          set: () => {
+            pass = false;
+          },
+        });
+
+        defaultsDeep(object, { a: value });
+        expect(pass).toBe(true);
+      });
+    });
+
+    it('should coerce primitives to objects', () => {
+      const primitives = [Boolean(), Number(), String()];
+
+      const expected = primitives.map(value => {
+        const object = Object(value);
+        object.a = 1;
+        return object;
+      });
+
+      const actual = primitives.map(value => {
+        return defaultsDeep(value as any, { a: 1 });
+      });
+
+      expect(actual).toEqual(expected);
+    });
+
+    it('should assign own and inherited string keyed source properties', () => {
+      function Foo(this: any) {
+        this.a = 1;
+      }
+      Foo.prototype.b = 2;
+
+      const expected = { a: 1, b: 2 };
+      // eslint-disable-next-line
+      // @ts-ignore
+      expect(defaultsDeep({}, new Foo())).toEqual(expected);
+    });
+
+    it('should not skip a trailing function source', () => {
+      function fn() {}
+      fn.b = 2;
+
+      expect(defaultsDeep({}, { a: 1 }, fn)).toEqual({ a: 1, b: 2 });
+    });
+
+    it('should not error on nullish sources', () => {
+      expect(defaultsDeep({ a: 1 }, undefined as any, { b: 2 }, null)).toEqual({
+        a: 1,
+        b: 2,
+      });
+    });
+
+    it('should create an object when `object` is nullish', () => {
+      const source = { a: 1 };
+      const values = [null, undefined];
+
+      const actual1 = values.map(value => {
+        const object = defaultsDeep(value as any, source);
+        return object !== source && object.a === 1;
+      });
+
+      expect(actual1).toEqual([true, true]);
+
+      const actual2 = values.map(value => {
+        return Object.keys(defaultsDeep(value as any)).length === 0;
+      });
+
+      expect(actual2).toEqual([true, true]);
+    });
+
+    it('should work as an iteratee for methods like reduce', () => {
+      const array = [{ a: 1 }, { b: 2 }, { c: 3 }];
+      const expected = { a: 0, b: 2, c: 3 };
+
+      function fn() {}
+      fn.a = array[0];
+      fn.b = array[1];
+      fn.c = array[2];
+
+      expect(array.reduce((result, value) => defaultsDeep(result, value), { a: 0 })).toEqual(expected);
+
+      const result = { a: 0 };
+      for (const key in fn) {
+        if (Object.hasOwn(fn, key)) {
+          defaultsDeep(result, (fn as any)[key as any] as any);
+        }
+      }
+      expect(result).toEqual(expected);
+    });
   });
 });
