034430335c18fec195866415abb7f678058e74bb
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2025-06-11 11:13:55 +0900

    test: remove import overhead (#1202)

--- benchmarks/performance/uniqueId.bench.ts
+++ benchmarks/performance/uniqueId.bench.ts
@@ -1,6 +1,9 @@
 import { bench, describe } from 'vitest';
-import { uniqueId as uniqueIdToolkitCompat } from 'es-toolkit/compat';
-import { uniqueId as uniqueIdLodash } from 'lodash';
+import { uniqueId as uniqueIdToolkitCompat_ } from 'es-toolkit/compat';
+import { uniqueId as uniqueIdLodash_ } from 'lodash';
+
+const uniqueIdToolkitCompat = uniqueIdToolkitCompat_;
+const uniqueIdLodash = uniqueIdLodash_;
 
 describe('uniqueId', () => {
   bench('es-toolkit/compat/uniqueId', () => {
