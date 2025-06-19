3bb28110262c338fbd061bdfc2bf232f3a9f6e1a
Sojin Park <raon0211@toss.im>
2025-06-19 23:14:45 +0900

    fix

--- src/compat/_internal/ValueIteratee.ts
+++ src/compat/_internal/ValueIteratee.ts
@@ -1,6 +1,5 @@
-export type ValueIteratee<T> =
-  | ((value: T) => unknown)
-  | PropertyKey
-  | readonly PropertyKey[]
-  | [PropertyKey | readonly PropertyKey[], any]
-  | Partial<T>;
+type PartialShallow<T> = {
+  [P in keyof T]?: T[P] extends object ? object : T[P];
+};
+
+export type ValueIteratee<T> = ((value: T) => unknown) | (PropertyKey | [PropertyKey, any] | PartialShallow<T>);
