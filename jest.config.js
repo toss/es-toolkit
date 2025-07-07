7dad30f42b5ef5064ae82363783c9c82b35c9fef
Sojin Park <raon0211@toss.im>
2025-07-07 12:43:42 +0900

    fix

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   moduleNameMapper: {
     '^vitest$': require.resolve('./jest.globals.js'),
   },
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+  },
   testMatch: ['<rootDir>/src/compat/**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
