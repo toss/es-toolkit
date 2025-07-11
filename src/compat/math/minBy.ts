124a97f346f45bc5bbdae5eabe9091ffbbc4fe24
Noritaka Kobayashi <arukimedesu1166777@gmail.com>
2025-07-12 00:14:05 +0900

    chore: fix typos

--- src/compat/math/minBy.ts
+++ src/compat/math/minBy.ts
@@ -4,18 +4,18 @@ import { ValueIteratee } from '../_internal/ValueIteratee.ts';
 import { iteratee as iterateeToolkit } from '../util/iteratee.ts';
 
 /**
- * Finds the element in an array that has the minium value when applying
+ * Finds the element in an array that has the minimum value when applying
  * the `iteratee` to each element.
  *
  * @template T - The type of elements in the array.
  * @param {T[]} items The array of elements to search.
  * @param {((element: T) => number) | keyof T | [keyof T, unknown] | Partial<T>} iteratee
- * The criteria used to determine the minium value.
+ * The criteria used to determine the minimum value.
  *  - If a **function** is provided, it extracts a numeric value from each element.
  *  - If a **string** is provided, it is treated as a key to extract values from the objects.
  *  - If a **[key, value]** pair is provided, it matches elements with the specified key-value pair.
  *  - If an **object** is provided, it matches elements that contain the specified properties.
- * @returns {T | undefined} The element with the minium value as determined by the `iteratee`.
+ * @returns {T | undefined} The element with the minimum value as determined by the `iteratee`.
  * @example
  * minBy([{ a: 1 }, { a: 2 }, { a: 3 }], x => x.a); // Returns: { a: 1 }
  * minBy([], x => x.a); // Returns: undefined
