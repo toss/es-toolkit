0d88b6e19d96e7b98e1dbfcc049d90ac9e4c6056
Wonsuk Choi <sukvvon@gmail.com>
2025-09-21 18:50:08 +0900

    chore(eslint.config): use 'defineConfig' for better type safety

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 import noForOfArrayPlugin from 'eslint-plugin-no-for-of-array';
 import prettier from 'eslint-plugin-prettier/recommended';
 import pluginVue from 'eslint-plugin-vue';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginJs from '@eslint/js';
 import vitest from '@vitest/eslint-plugin';
 
-export default [
+export default defineConfig(
   {
     ignores: [
       '.yarn/**',
@@ -135,5 +136,5 @@ export default [
       'vue/multi-word-component-names': 'off',
       'prefer-object-has-own': 'error',
     },
-  },
-];
+  }
+);
