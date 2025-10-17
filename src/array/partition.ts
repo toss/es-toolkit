b829c97c450ea5710a9f8b5ebf5a8977561ea21f
Noah Schmalz <31627472+the5thbeatle@users.noreply.github.com>
2025-10-17 01:13:42 -0500

    feat(partition): Type guard support (#1471)
    * Adds support for type guards in partition
    
    * Excludes type guard type from return type of partition

--- src/array/partition.ts
+++ src/array/partition.ts
@@ -6,6 +6,34 @@
  * the second array contains elements for which the predicate function returns false.
  *
  * @template T - The type of elements in the array.
+ * @template {T} U - The type being filtered for.
+ * @param {T[]} arr - The array to partition.
+ * @param {(value: T) => value is U} isInTruthy - A type guard that determines whether an
+ * element should be placed in the truthy array. The function is called with each element
+ * of the array.
+ * @returns {[U[], Exclude<T, U>[]]} A tuple containing two arrays: the first array contains elements for
+ * which the predicate returned true, and the second array contains elements for which the
+ * predicate returned false.
+ *
+ * @example
+ * const array = [1, 2, 3, 4] as const;
+ * const isEven = (x: number): x is 2 | 4 => x % 2 === 0;
+ * const [even, odd]: [(2 | 4)[], (2 | 4)[]] = partition(array, isEven);
+ * // even will be [2, 4], and odd will be [1, 3]
+ */
+export function partition<T, U extends T>(
+  arr: readonly T[],
+  isInTruthy: (value: T) => value is U
+): [truthy: U[], falsy: Array<Exclude<T, U>>];
+
+/**
+ * Splits an array into two groups based on a predicate function.
+ *
+ * This function takes an array and a predicate function. It returns a tuple of two arrays:
+ * the first array contains elements for which the predicate function returns true, and
+ * the second array contains elements for which the predicate function returns false.
+ *
+ * @template T - The type of elements in the array.
  * @param {T[]} arr - The array to partition.
  * @param {(value: T) => boolean} isInTruthy - A predicate function that determines
  * whether an element should be placed in the truthy array. The function is called with each
@@ -20,6 +48,7 @@
  * const [even, odd] = partition(array, isEven);
  * // even will be [2, 4], and odd will be [1, 3, 5]
  */
+export function partition<T>(arr: readonly T[], isInTruthy: (value: T) => boolean): [truthy: T[], falsy: T[]];
 export function partition<T>(arr: readonly T[], isInTruthy: (value: T) => boolean): [truthy: T[], falsy: T[]] {
   const truthy: T[] = [];
   const falsy: T[] = [];
