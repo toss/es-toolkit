309b2d1891b0413d299043297e938f2ab38c7db7
Sojin Park <raon0211@toss.im>
2025-12-12 16:02:33 +0900

    refactor

--- src/object/merge.ts
+++ src/object/merge.ts
@@ -57,18 +57,20 @@ export function merge<T extends Record<PropertyKey, any>, S extends Record<Prope
     const sourceValue = source[key];
     const targetValue = target[key];
 
-    if (Array.isArray(sourceValue) || isPlainObject(sourceValue)) {
-      if (Array.isArray(targetValue) || isPlainObject(targetValue)) {
-        target[key] = merge(targetValue, sourceValue);
-      } else if (Array.isArray(sourceValue)) {
-        target[key] = merge([], sourceValue);
-      } else {
-        target[key] = merge({}, sourceValue);
-      }
+    if (isMergeableValue(sourceValue) && isMergeableValue(targetValue)) {
+      target[key] = merge(targetValue, sourceValue);
+    } else if (Array.isArray(sourceValue)) {
+      target[key] = merge([], sourceValue);
+    } else if (isPlainObject(sourceValue)) {
+      target[key] = merge({}, sourceValue);
     } else if (targetValue === undefined || sourceValue !== undefined) {
       target[key] = sourceValue;
     }
   }
 
   return target;
+}
+
+function isMergeableValue(value: unknown) {
+  return isPlainObject(value) || Array.isArray(value);
 }
