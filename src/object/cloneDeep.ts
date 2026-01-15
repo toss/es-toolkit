a6fd27eed369b4bb94cff1e8cd05161bfcb0b7e5
eunwoo-levi <eunwoo1341@gmail.com>
2026-01-16 02:29:17 +0900

    docs[cloneDeep]: Fix incorrect function names and output in JSDoc examples

--- src/object/cloneDeep.ts
+++ src/object/cloneDeep.ts
@@ -10,37 +10,37 @@ import { cloneDeepWithImpl } from './cloneDeepWith.ts';
  * @example
  * // Clone a primitive values
  * const num = 29;
- * const clonedNum = clone(num);
+ * const clonedNum = cloneDeep(num);
  * console.log(clonedNum); // 29
  * console.log(clonedNum === num); // true
  *
  * @example
  * // Clone an array
  * const arr = [1, 2, 3];
- * const clonedArr = clone(arr);
+ * const clonedArr = cloneDeep(arr);
  * console.log(clonedArr); // [1, 2, 3]
  * console.log(clonedArr === arr); // false
  *
  * @example
  * // Clone an array with nested objects
  * const arr = [1, { a: 1 }, [1, 2, 3]];
- * const clonedArr = clone(arr);
+ * const clonedArr = cloneDeep(arr);
  * arr[1].a = 2;
- * console.log(arr); // [2, { a: 2 }, [1, 2, 3]]
+ * console.log(arr); // [1, { a: 2 }, [1, 2, 3]]
  * console.log(clonedArr); // [1, { a: 1 }, [1, 2, 3]]
  * console.log(clonedArr === arr); // false
  *
  * @example
  * // Clone an object
  * const obj = { a: 1, b: 'es-toolkit', c: [1, 2, 3] };
- * const clonedObj = clone(obj);
+ * const clonedObj = cloneDeep(obj);
  * console.log(clonedObj); // { a: 1, b: 'es-toolkit', c: [1, 2, 3] }
  * console.log(clonedObj === obj); // false
  *
  * @example
  * // Clone an object with nested objects
  * const obj = { a: 1, b: { c: 1 } };
- * const clonedObj = clone(obj);
+ * const clonedObj = cloneDeep(obj);
  * obj.b.c = 2;
  * console.log(obj); // { a: 1, b: { c: 2 } }
  * console.log(clonedObj); // { a: 1, b: { c: 1 } }
