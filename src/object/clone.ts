d723dcf42060cba9f17a21bf1ceeb8dec3051c22
Matt Oakes <hello@mattoakes.net>
2026-01-15 08:01:55 +0000

    fix(clone): Fix error when cloning object with null prototype (#1570)

--- src/object/clone.ts
+++ src/object/clone.ts
@@ -44,7 +44,7 @@ export function clone<T>(obj: T): T {
   }
 
   const prototype = Object.getPrototypeOf(obj);
-  const Constructor = prototype.constructor;
+  const Constructor = prototype?.constructor;
 
   if (obj instanceof Date || obj instanceof Map || obj instanceof Set) {
     return new Constructor(obj);
