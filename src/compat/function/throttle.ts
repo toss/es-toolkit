d051649741fdfb5f477d6d8cc26bda8b137755ec
Sojin Park <raon0211@toss.im>
2026-01-23 15:58:31 +0900

    revert: Revert "fix(throttle): prevent maxWait from being set when both edges are disabled (#1560)"
    This reverts commit 79338c98a1bc8660af9df0da0e7acdee556c7b6c.

--- src/compat/function/throttle.ts
+++ src/compat/function/throttle.ts
@@ -126,7 +126,7 @@ export function throttle<F extends (...args: any[]) => any>(
 
   return debounce(func, throttleMs, {
     leading,
-    maxWait: leading || trailing ? throttleMs : undefined,
+    maxWait: throttleMs,
     trailing,
   });
 }
