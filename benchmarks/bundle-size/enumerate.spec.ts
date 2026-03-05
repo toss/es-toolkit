75e7d365884ca0b3e9c2799cbe813b0537b7c158
y0ngha <dev.y0ngha@gmail.com>
2026-03-06 03:59:35 +0900

    test: chain, enumerate, zipIterable bundle size add

---
+++ benchmarks/bundle-size/enumerate.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest';
+import { getBundleSize } from './utils/getBundleSize';
+
+describe('enumerate bundle size', () => {
+  it('es-toolkit', async () => {
+    const bundleSize = await getBundleSize('es-toolkit/iterator', 'enumerate');
+    expect(bundleSize).toMatchInlineSnapshot(`82`);
+  });
+
+  it('@fxts/core', async () => {
+    const bundleSize = await getBundleSize('@fxts/core', 'zipWithIndex');
+    expect(bundleSize).toMatchInlineSnapshot(`2710`);
+  });
+});
