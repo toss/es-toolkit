eb1cdc71d43b1ce4787f810396f31982aa6c1578
D-Sketon <2055272094@qq.com>
2026-03-15 23:12:01 +0800

    fix(compat/isNaN): improve NaN checks for boxed values and non-numbers

--- src/compat/predicate/isNaN.spec.ts
+++ src/compat/predicate/isNaN.spec.ts
@@ -9,6 +9,11 @@ describe('isNaN', () => {
 
   it('should return `false` for non-NaN numbers', () => {
     expect(isNaN(0)).toBe(false);
+    expect(isNaN(new Number(0))).toBe(false);
+  });
+
+  it('should return `true` for boxed NaN', () => {
+    expect(isNaN(new Number(NaN))).toBe(true);
   });
 
   it('should return `false` for non-numbers', () => {
