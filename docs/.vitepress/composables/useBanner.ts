98420c23ef575eec3c876883e4bc9583371784d2
Antoliny0919 <antoliny0919@gmail.com>
2026-05-30 12:00:40 +0900

    docs(banner): add ja and zh-hand versions banner

--- docs/.vitepress/composables/useBanner.ts
+++ docs/.vitepress/composables/useBanner.ts
@@ -1,5 +1,5 @@
 import { computed, onMounted, onUnmounted, ref } from 'vue';
-import { type Banner, EN_BANNER_DATA, KO_BANNER_DATA } from '../data/bannerData';
+import { type Banner, EN_BANNER_DATA, JA_BANNER_DATA, KO_BANNER_DATA, ZH_HANS_BANNER_DATA } from '../data/bannerData';
 
 export function useBanner() {
   const currentLang = ref<string | null>(null);
@@ -7,9 +7,19 @@ export function useBanner() {
     if (currentLang.value === 'ko') {
       return KO_BANNER_DATA;
     }
+
     if (currentLang.value === 'en') {
       return EN_BANNER_DATA;
     }
+
+    if (currentLang.value === 'ja') {
+      return JA_BANNER_DATA;
+    }
+
+    if (currentLang.value === 'zh-hans') {
+      return ZH_HANS_BANNER_DATA;
+    }
+
     return [];
   });
 
@@ -117,8 +127,14 @@ const getCurrentLang = (): string | null => {
   if (path.startsWith('/ko/')) {
     return 'ko';
   }
-  if (path.startsWith('/zh_hans/') || path.startsWith('/ja/')) {
-    return null;
+
+  if (path.startsWith('/zh_hans/')) {
+    return 'zh-hans';
   }
+
+  if (path.startsWith('/ja/')) {
+    return 'ja';
+  }
+
   return 'en';
 };
