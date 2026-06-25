1155f017a2c02f9fe31c8504dfe67633d69d98f9
BangDori <44726494+BangDori@users.noreply.github.com>
2026-06-25 23:11:10 +0900

    test(capitalize): preserve original test description

--- src/compat/string/capitalize.spec.ts
+++ src/compat/string/capitalize.spec.ts
@@ -3,19 +3,16 @@ import type { capitalize as capitalizeLodash } from 'lodash';
 import { capitalize } from './capitalize';
 
 describe('capitalize', () => {
-  it('should capitalize the first character', () => {
+  it('should capitalize the first character of a string', () => {
     expect(capitalize('fred')).toBe('Fred');
     expect(capitalize('Fred')).toBe('Fred');
+    expect(capitalize(' fred')).toBe(' fred');
+    expect(capitalize(' fred  ')).toBe(' fred  ');
   });
 
   it('should lowercase the remaining characters', () => {
     expect(capitalize('FRED')).toBe('Fred');
     expect(capitalize('FOO BAR')).toBe('Foo bar');
-  });
-
-  it('should not trim leading whitespace', () => {
-    expect(capitalize(' fred')).toBe(' fred');
-    expect(capitalize('  fred')).toBe('  fred');
   });
 
   it('should return an empty string for an empty string', () => {
