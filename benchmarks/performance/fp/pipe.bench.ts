a3b1d38df3b2713351501817b51a0fd76e2badf9
Sojin Park <raon0211@toss.im>
2026-06-25 09:25:40 +0900

    test(fp): compare lodash pipeline performance

--- benchmarks/performance/fp/pipe.bench.ts
+++ benchmarks/performance/fp/pipe.bench.ts
@@ -4,23 +4,29 @@ import {
   filter as filterToolkit,
   map as mapToolkit,
   pipe as pipeToolkit,
-  sortBy as sortByToolkit,
   take as takeToolkit,
   uniq as uniqToolkit,
 } from 'es-toolkit/fp';
+import lodashFp from 'lodash/fp';
 import {
   chunk as chunkRemeda,
   filter as filterRemeda,
   map as mapRemeda,
   pipe as pipeRemeda,
-  sortBy as sortByRemeda,
   take as takeRemeda,
   unique as uniqueRemeda,
 } from 'remeda';
 
 const array = Array.from({ length: 10000 }, (_, i) => i);
 const duplicated = Array.from({ length: 10000 }, (_, i) => i % 1000);
-const objects = array.map(value => ({ value: (value * 7919) % 10000 }));
+const {
+  chunk: chunkLodash,
+  filter: filterLodash,
+  map: mapLodash,
+  pipe: pipeLodash,
+  take: takeLodash,
+  uniq: uniqLodash,
+} = lodashFp;
 
 describe('pipe/map', () => {
   bench('es-toolkit/fp', () => {
@@ -35,6 +41,10 @@ describe('pipe/map', () => {
       array,
       mapRemeda(x => x * 2)
     );
+  });
+
+  bench('lodash/fp', () => {
+    pipeLodash(mapLodash((x: number) => x * 2))(array);
   });
 });
 
@@ -53,6 +63,13 @@ describe('pipe/filter + map', () => {
       filterRemeda(x => x % 2 === 0),
       mapRemeda(x => x + 1)
     );
+  });
+
+  bench('lodash/fp', () => {
+    pipeLodash(
+      filterLodash((x: number) => x % 2 === 0),
+      mapLodash((x: number) => x + 1)
+    )(array);
   });
 });
 
@@ -74,6 +91,14 @@ describe('pipe/map + filter + take(5) (lazy short-circuit)', () => {
       takeRemeda(5)
     );
   });
+
+  bench('lodash/fp', () => {
+    pipeLodash(
+      mapLodash((x: number) => x * 2),
+      filterLodash((x: number) => x % 3 === 0),
+      takeLodash(5)
+    )(array);
+  });
 });
 
 describe('pipe/uniq', () => {
@@ -83,6 +108,10 @@ describe('pipe/uniq', () => {
 
   bench('remeda', () => {
     pipeRemeda(duplicated, uniqueRemeda());
+  });
+
+  bench('lodash/fp', () => {
+    pipeLodash(uniqLodash)(duplicated);
   });
 });
 
@@ -94,17 +123,8 @@ describe('pipe/chunk(10)', () => {
   bench('remeda', () => {
     pipeRemeda(array, chunkRemeda(10));
   });
-});
 
-describe('pipe/sortBy', () => {
-  bench('es-toolkit/fp', () => {
-    pipeToolkit(objects, sortByToolkit([item => item.value]));
-  });
-
-  bench('remeda', () => {
-    pipeRemeda(
-      objects,
-      sortByRemeda(item => item.value)
-    );
+  bench('lodash/fp', () => {
+    pipeLodash(chunkLodash(10))(array);
   });
 });
