be60b146e0e2c7940eb7e13bfc2a1697809326b1
minseonkkim <minsun9856@gmail.com>
2026-07-21 17:27:33 +0900

    chore: update bundle size snapshots

--- benchmarks/bundle-size/merge.spec.ts
+++ benchmarks/bundle-size/merge.spec.ts
@@ -14,6 +14,6 @@ describe('merge bundle size', () => {
 
   it('es-toolkit/compat', async () => {
     const bundleSize = await getBundleSize('es-toolkit/compat', 'merge');
-    expect(bundleSize).toMatchInlineSnapshot(`6377`);
+    expect(bundleSize).toMatchInlineSnapshot(`6381`);
   });
 });
