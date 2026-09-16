4b656078ecb2c609a2b086c1e8504bada23709c0
김보경 <rlaqhrud6263@naver.com>
2026-09-17 02:32:49 +0900

    test: update bundle size snapshots

--- benchmarks/bundle-size/fp/countBy.spec.ts
+++ benchmarks/bundle-size/fp/countBy.spec.ts
@@ -5,6 +5,6 @@ describe('fp/countBy bundle size', () => {
   it('es-toolkit/fp', async () => {
     expect(
       await getBundleSizeFromScript('import { countBy } from "es-toolkit/fp"; console.log(countBy)')
-    ).toMatchInlineSnapshot(`176`);
+    ).toMatchInlineSnapshot(`296`);
   });
 });
